06ab3451945facbf5d0add062db56f830075eaa6
olaf <olaf>
2001-01-09 18:50:24 +0000

    reverse order of atexit stack

--- lib/atexit.c
+++ lib/atexit.c
@@ -15,9 +15,9 @@ int atexit(function t) {
 extern void _exit(int code) __attribute__((noreturn));
 
 void exit(int code) {
-  if (__atexitlist[0]) __atexitlist[0]();
-  if (__atexitlist[1]) __atexitlist[1]();
-  if (__atexitlist[2]) __atexitlist[2]();
   if (__atexitlist[3]) __atexitlist[3]();
+  if (__atexitlist[2]) __atexitlist[2]();
+  if (__atexitlist[1]) __atexitlist[1]();
+  if (__atexitlist[0]) __atexitlist[0]();
   _exit(code);
 }
