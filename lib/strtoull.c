0a1ada03923e4a1176794846573d297a5b25b27f
fefe <fefe>
2001-01-16 15:01:18 +0000

    optimized strtoul and strtoull.  They are much better now. Don't look at the old versions, please!  (Yuck!) ;-)

--- lib/strtoull.c
+++ lib/strtoull.c
@@ -1,38 +1,29 @@
 #include <ctype.h>
 
-static char *num="0123456789abcdefghijklmnopqrstuvwxyz";
-
 unsigned long long int strtoull(const char *nptr, char **endptr, int base)
 {
   long long int v=0;
-  int a=1,i;
 
-  while(isspace(*nptr)) nptr++;
+  while(isspace(*nptr)) ++nptr;
 
-  if (*nptr == '+') { nptr++; }
-  if (!base)
-  {
-    if (*nptr=='0')
-    {
+  if (*nptr == '+') ++nptr;
+  if (!base) {
+    if (*nptr=='0') {
       base=8;
-      if ((*(nptr+1)=='x')||(*(nptr+1)=='X')) base=16;
+      if ((*(nptr+1)=='x')||(*(nptr+1)=='X')) {
+	nptr+=2;
+	base=16;
+      }
     }
     else
       base=10;
   }
-  while((a)&&(*nptr))
-  {
-    a=0;
-    for (i=0;(!a)&&(i<base);i++)
-    {
-      if ((*nptr == num[i]) || (*nptr == (num[i]-' ')))
-      {
-	a=1;
-	v=(v*base)+(&num[i]-num);
-      }
-    }
-    if ((!a)&&(base==16)&&((*nptr=='x')||(*nptr=='X'))) a=1;
-    if (a) nptr++;
+  while(*nptr) {
+    register char c=*nptr;
+    c=(c>='a'?c-'a'+10:c>='A'?c-'A'+10:c-'0');
+    if (c>=base) break;
+    v=v*base+c;
+    ++nptr;
   }
   if (endptr) *endptr=(char *)nptr;
   return v;
