9a1aa6ffda7094745d35b4e5c7a557bb71fb37a8
olaf <olaf>
2001-01-18 11:37:09 +0000

    Ups... added a mutex to strtok... shame on me

--- lib/strtok.c
+++ lib/strtok.c
@@ -1,15 +1,8 @@
-#include <pthread.h>
-
 char *strtok_r(char *s, const char *delim, char **ptrptr);
 
 static char *strtok_pos;
-static pthread_mutex_t mutex_strtok;
 
 char *strtok(char *s, const char *delim)
 {
-  char* ret;
-  pthread_mutex_lock(&mutex_strtok);
-  ret=strtok_r(s,delim,&strtok_pos);
-  pthread_mutex_unlock(&mutex_strtok);
-  return ret;
+  return strtok_r(s,delim,&strtok_pos);
 }
