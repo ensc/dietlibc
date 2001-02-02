cac9ef7ac8d1c009a2a1fe725e27c53b1f0171da
fefe <fefe>
2001-02-02 21:46:13 +0000

    argl, open64 collided with the unistd.h prototype which uses ,....

--- lib/open64.c
+++ lib/open64.c
@@ -1,12 +1,14 @@
 #include <linux/types.h>
 #include <linux/fcntl.h>
-#include <unistd.h>
 
 #ifndef O_LARGEFILE
 #define O_LARGEFILE 0
 #endif
-int open64(const char* file,int oflag,int mode) {
+
+extern int open(const char* file,int oflag,int mode);
+
+int __libc_open64(const char* file,int oflag,int mode) {
   return open(file,oflag|O_LARGEFILE,mode);
 }
 
-int __libc_open64(const char* file,int oflag,int mode) __attribute__((weak,alias("open64")));
+int open64(const char* file,int oflag,int mode) __attribute__((weak,alias("__libc_open64")));
