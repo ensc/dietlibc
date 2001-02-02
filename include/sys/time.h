e0ce74939fcf61771c1d29765ff75d09129723f8
fefe <fefe>
2001-02-02 22:03:46 +0000

    moving around some includes to be more BSD like.

--- include/sys/time.h
+++ include/sys/time.h
@@ -21,4 +21,26 @@ int settimeofday(const struct timeval *tv , const struct timezone *tz) __THROW;
 extern int adjtime (const struct timeval *__delta,
 		    struct timeval *__olddelta) __THROW;
 
+struct tm
+{
+  int tm_sec;			/* Seconds.	[0-60] (1 leap second) */
+  int tm_min;			/* Minutes.	[0-59] */
+  int tm_hour;			/* Hours.	[0-23] */
+  int tm_mday;			/* Day.		[1-31] */
+  int tm_mon;			/* Month.	[0-11] */
+  int tm_year;			/* Year	- 1900.  */
+  int tm_wday;			/* Day of week.	[0-6] */
+  int tm_yday;			/* Days in year.[0-365]	*/
+  int tm_isdst;			/* DST.		[-1/0/1]*/
+
+  long int tm_gmtoff;		/* Seconds east of UTC.  */
+  const char *tm_zone;		/* Timezone abbreviation.  */
+};
+
+struct tm* gmtime(const time_t *t) __THROW;
+struct tm* gmtime_r(const time_t *t, struct tm *r) __THROW;
+
+struct tm* localtime(const time_t *t) __THROW;
+struct tm* localtime_r(const time_t *t, struct tm *r) __THROW;
+
 #endif
--- include/time.h
+++ include/time.h
@@ -4,38 +4,14 @@
 #include <sys/cdefs.h>
 #include <sys/time.h>
 
-struct tm
-{
-  int tm_sec;			/* Seconds.	[0-60] (1 leap second) */
-  int tm_min;			/* Minutes.	[0-59] */
-  int tm_hour;			/* Hours.	[0-23] */
-  int tm_mday;			/* Day.		[1-31] */
-  int tm_mon;			/* Month.	[0-11] */
-  int tm_year;			/* Year	- 1900.  */
-  int tm_wday;			/* Day of week.	[0-6] */
-  int tm_yday;			/* Days in year.[0-365]	*/
-  int tm_isdst;			/* DST.		[-1/0/1]*/
-
-  long int tm_gmtoff;		/* Seconds east of UTC.  */
-  const char *tm_zone;		/* Timezone abbreviation.  */
-};
-
 extern int __isleap(int year);
 
 int nanosleep(const struct timespec *req, struct timespec *rem) __THROW;
 unsigned int sleep(unsigned int secs) __THROW;
 
-struct tm *localtime(const time_t *timep) __THROW;
-
 time_t mktime(struct tm *timeptr) __THROW __pure__;
 
 char *asctime(const struct tm *timeptr) __THROW;
 char *asctime_r(const struct tm *timeptr, char *buf) __THROW;
-
-struct tm* gmtime(const time_t *t) __THROW;
-struct tm* gmtime_r(const time_t *t, struct tm *r) __THROW;
-
-struct tm* localtime(const time_t *t) __THROW;
-struct tm* localtime_r(const time_t *t, struct tm *r) __THROW;
 
 #endif
