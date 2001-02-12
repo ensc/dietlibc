daf964c2b89b4bfc26e156604ebff9cca7afb562
fefe <fefe>
2001-02-12 22:41:50 +0000

    fixed fgets return value (always returned "").

--- libstdio/fgets.c
+++ libstdio/fgets.c
@@ -3,6 +3,7 @@
 extern int ferror(FILE *stream);
 
 char *fgets(char *s, int size, FILE *stream) {
+  char *orig=s;
   int l;
   for (l=size; l>0;) {
     int c=fgetc(stream);
@@ -15,5 +16,5 @@ char *fgets(char *s, int size, FILE *stream) {
   if (l==size || ferror(stream))
     return 0;
   *s=0;
-  return s;
+  return orig;
 }
