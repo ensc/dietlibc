58e2fd1506e2e32b9ad12cfee1b6ebb50ee92c66
fefe <fefe>
2001-03-04 21:39:36 +0000

    shmat kludge

--- sparc/shmat.c
+++ sparc/shmat.c
@@ -5,11 +5,15 @@
 
 extern void* __ipc();
 
+#ifndef PAGE_SIZE
+#define PAGE_SIZE 4096
+#endif
+
 void* shmat(int shmid,const void* shmaddr,int shmflg) {
   void* raddr;
   register void* result;
   result=__ipc(SHMAT,shmid,shmflg,&raddr,shmaddr);
-  if ((unsigned long)result <= -(unsigned long)SHMLBA)
+  if ((unsigned long)result <= -(unsigned long)PAGE_SIZE)
     result=raddr;
   return result;
 }
