9145db65ff621307ef17625e8ce04aabf336a4fd
olaf <olaf>
2001-03-14 13:37:27 +0000

    some macros for wait{,3,4,pid} status

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -5,6 +5,25 @@
 #include <sys/resource.h>
 #include <linux/wait.h>
 
+/* If WIFEXITED(STATUS), the low-order 8 bits of the status.  */
+#define __WEXITSTATUS(status)	(((status) & 0xff00) >> 8)
+
+/* If WIFSIGNALED(STATUS), the terminating signal.  */
+#define __WTERMSIG(status)	((status) & 0x7f)
+
+/* If WIFSTOPPED(STATUS), the signal that stopped the child.  */
+#define __WSTOPSIG(status)	__WEXITSTATUS(status)
+
+/* Nonzero if STATUS indicates normal termination.  */
+#define WIFEXITED(status)	(__WTERMSIG(status) == 0)
+
+/* Nonzero if STATUS indicates termination by a signal.  */
+#define WIFSIGNALED(status)	(!WIFSTOPPED(status) && !WIFEXITED(status))
+
+/* Nonzero if STATUS indicates the child is stopped.  */
+#define WIFSTOPPED(status)	(((status) & 0xff) == 0x7f)
+
+
 pid_t wait(int *status) __THROW;
 pid_t waitpid(pid_t pid, int *status, int options) __THROW;
 
