3d03b04d4f69f5f37093beaffc9e237d1b16a1d1
olaf <olaf>
2001-03-15 01:58:06 +0000

    aeh missed to implement pthread_exit.

---
+++ libpthread/pthread_exit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <errno.h>
+
+#include <pthread.h>
+#include "thread_internal.h"
+
+void pthread_exit(void *retval)
+{
+  _pthread_descr this;
+
+  __THREAD_INIT();
+
+  this = __thread_self();
+  this->retval = retval;
+
+  _exit(0);
+}
