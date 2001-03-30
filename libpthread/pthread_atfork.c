e8721723665b8bf66929a41355a1718527644906
olaf <olaf>
2001-03-30 17:34:10 +0000

    ok added a lock for save atfork-stack usage...

--- libpthread/pthread_atfork.c
+++ libpthread/pthread_atfork.c
@@ -6,6 +6,7 @@
 
 #define PTHREAD_NUM_ATFORK 4
 
+static struct _pthread_fastlock __atfork_struct_lock;
 static struct {
   int used;
   void (*prepare)(void);
@@ -17,17 +18,26 @@ int pthread_atfork(void (*prepare)(void),
 		   void (*parent)(void),
 		   void (*child)(void))
 {
-  int i;
-  for (i=0;i<PTHREAD_NUM_ATFORK; ++i)
-    if (!(pthread_atfork_buf[i].used))
-      if (__testandset(&pthread_atfork_buf[i].used)) {
-	pthread_atfork_buf[i].prepare=prepare;
-	pthread_atfork_buf[i].parent=parent;
-	pthread_atfork_buf[i].child=child;
-	return 0;
-      }
-  (*(__errno_location()))=ENOMEM;
-  return -1;
+  int i,ret=-1;
+
+  __NO_ASYNC_CANCEL_BEGIN;
+  __pthread_lock(&__atfork_struct_lock);
+
+  for (i=0;i<PTHREAD_NUM_ATFORK; ++i) {
+    if (!(pthread_atfork_buf[i].used)) {
+      pthread_atfork_buf[i].prepare=prepare;
+      pthread_atfork_buf[i].parent=parent;
+      pthread_atfork_buf[i].child=child;
+      ret=0;
+      break;
+    }
+  }
+
+  __pthread_unlock(&__atfork_struct_lock);
+  __NO_ASYNC_CANCEL_END;
+
+  if (ret) (*(__errno_location()))=ENOMEM;
+  return ret;
 }
 
 pid_t fork(void)
@@ -35,27 +45,33 @@ pid_t fork(void)
   pid_t pid;
   int i=PTHREAD_NUM_ATFORK;
 
-  __TEST_CANCEL();
+  __NO_ASYNC_CANCEL_BEGIN;
 
-  do {
-    --i;
-    if (pthread_atfork_buf[i].used)
+  __TEST_CANCEL();
+  __pthread_lock(&__atfork_struct_lock);
+
+  while (i) {
+    if (pthread_atfork_buf[--i].used)
       if (pthread_atfork_buf[i].prepare)
 	pthread_atfork_buf[i].prepare();
-  } while (i>0);
+  }
 
   pid=__libc_fork();
-  if (pid>0) {
+
+  if (pid>0) {		/* parent */
     for (i=0;i<PTHREAD_NUM_ATFORK;++i)
       if (pthread_atfork_buf[i].used)
 	if (pthread_atfork_buf[i].parent)
 	  pthread_atfork_buf[i].parent();
-  } else if (pid==0) {
+  } else if (pid==0) {	/* child */
     for (i=0;i<PTHREAD_NUM_ATFORK;++i)
       if (pthread_atfork_buf[i].used)
 	if (pthread_atfork_buf[i].child)
 	  pthread_atfork_buf[i].child();
   }
+
+  __pthread_unlock(&__atfork_struct_lock);
+  __NO_ASYNC_CANCEL_END;
 
   return pid;
 }
