b193a4ba5fb4326542c9b7fb376fb313731e19bd
fefe <fefe>
2001-04-09 15:39:23 +0000

    more alpha time(3) testing.

--- alpha/__time.c
+++ alpha/__time.c
@@ -5,7 +5,7 @@ time_t time(time_t *foo) {
   struct timeval tv;
   time_t tmp=(time_t)-1;
   if (gettimeofday(&tv,0)==0)
-    result=(time_t)tv.tv_sec;
-  if (foo) *foo=result;
-  return result;
+    tmp=(time_t)tv.tv_sec;
+  if (foo) *foo=tmp;
+  return tmp;
 }
