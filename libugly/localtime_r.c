ed6ceb91a6cc3653b2782017340839d2c6be0c76
fefe <fefe>
2001-04-17 23:56:40 +0000

    localtime won't call gettimeofday when the tzfile parser is used.

--- libugly/localtime_r.c
+++ libugly/localtime_r.c
@@ -8,13 +8,13 @@ extern time_t __tzfile_map(time_t t, int *isdst);
 #endif
 
 struct tm* localtime_r(const time_t* t, struct tm* r) {
-  struct timezone tz;
   time_t tmp;
-  gettimeofday(0, &tz);
 #ifdef WANT_TZFILE_PARSER
   __maplocaltime();
   tmp=__tzfile_map(*t,&r->tm_isdst);
 #else
+  struct timezone tz;
+  gettimeofday(0, &tz);
   tmp=*t-tz.tz_minuteswest*60L;
 #endif
   return gmtime_r(&tmp,r);
