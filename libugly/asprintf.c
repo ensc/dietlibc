e9787a87265d2d4190b1e811816260238c3c88c0
fefe <fefe>
2001-05-05 12:27:15 +0000

    add asprintf.

---
+++ libugly/asprintf.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <linux/types.h>
+#include <stdlib.h>
+#include "dietwarning.h"
+
+extern int vsnprintf (char *str,size_t size,const char *format, va_list arg_ptr);
+
+int asprintf(char **s, const char *format,...)
+{
+  int n;
+  va_list arg_ptr;
+  va_start(arg_ptr, format);
+  n=vsnprintf(0,1000000,format,arg_ptr);
+  va_end (arg_ptr);
+  va_start (arg_ptr, format);
+  if ((*s=malloc(n+2))) {
+    n=vsnprintf(*s,n+1,format,arg_ptr);
+    va_end(arg_ptr);
+    return n;
+  }
+  return -1;
+}
