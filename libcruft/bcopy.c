cd26892d19576eae7fa9579a87adce945b6eda72
fefe <fefe>
2001-05-11 15:20:42 +0000

    fix convenience wrapper so it actually compiles.

--- libcruft/bcopy.c
+++ libcruft/bcopy.c
@@ -1,7 +1,10 @@
+#include <sys/types.h>
 #include "dietwarning.h"
+
+extern void memmove(char*,const char*,size_t);
 
 void bcopy(const void *src, void *dest, size_t n) {
   memmove(dest,src,n);
 }
 
-link_warning(bcopy,"warning: you used bcopy without including <string.h>!")
+link_warning("bcopy","warning: you used bcopy without including <string.h>!")
