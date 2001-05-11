c1c84686e11f2f78de66a5bb0693219d875070b2
fefe <fefe>
2001-05-11 13:12:52 +0000

    fix fdglue thread safeness

---
+++ libpthread/pthread_fdglue2.c
@@ -0,0 +1,36 @@
+#include "dietstdio.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#ifdef WANT_BUFFERED_STDIO
+extern int __stdio_atexit;
+extern void __stdio_flushall();
+#endif
+
+FILE* __stdio_init_file(int fd) {
+  FILE *tmp=(FILE*)malloc(sizeof(FILE));
+  if (!tmp) {
+    close(fd);
+    errno=ENOMEM;
+    return 0;
+  }
+  tmp->fd=fd;
+#ifdef WANT_BUFFERED_STDIO
+  tmp->bm=0;
+  tmp->bs=0;
+#endif
+  tmp->flags=0;
+#ifdef WANT_BUFFERED_STDIO
+  if (__stdio_atexit==0) {
+    __stdio_atexit=1;
+    atexit(__stdio_flushall);
+  }
+  tmp->next=__stdio_root;
+  __stdio_root=tmp;
+#endif
+  pthread_mutex_init(&tmp->m,0);
+  return tmp;
+}
--- libstdio/fdglue.c
+++ libstdio/fdglue.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #ifdef WANT_BUFFERED_STDIO
 extern int __stdio_atexit;
@@ -24,29 +25,3 @@ int __stdio_parse_mode(const char *mode) {
   }
 }
 
-FILE* __stdio_init_file(int fd) {
-  FILE *tmp=(FILE*)malloc(sizeof(FILE));
-  if (!tmp) {
-    close(fd);
-    errno=ENOMEM;
-    return 0;
-  }
-  tmp->fd=fd;
-#ifdef WANT_BUFFERED_STDIO
-  tmp->bm=0;
-  tmp->bs=0;
-#endif
-  tmp->flags=0;
-#ifdef WANT_BUFFERED_STDIO
-  if (__stdio_atexit==0) {
-    __stdio_atexit=1;
-    atexit(__stdio_flushall);
-  }
-  tmp->next=__stdio_root;
-  __stdio_root=tmp;
-#endif
-#ifdef WANT_THREAD_SAFE
-  pthread_mutex_init(&tmp->m,0);
-#endif
-  return tmp;
-}
