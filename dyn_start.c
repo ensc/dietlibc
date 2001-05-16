cabdcf3c4205f0b198a739b116c7e31bb02c00cb
olaf <olaf>
2001-05-16 13:43:57 +0000

    added dynamic con- and destructor, start/stop code... this is used in:  - other libs with pre- and post-main-code  - shared objects pre- and post-main-code  - c++ (static initialiser of instanced classes)    (little test c++ program written :) )

---
+++ dyn_start.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+typedef void(*structor)(void);
+
+#if 0 
+
+/* OLD tricks are this still needed ? */
+
+/* dirty trick to force gcc to be in the data section */
+static structor force_to_data[0] = {};
+
+/* next dirty tricks to make program/sharedlib "local" objects and functions */
+
+asm ( ".section .ctors,\"aw\" "); /* now change the section without gcc knowing it ! */
+static structor __CTOR_LIST__[1]={((structor)-1)};
+
+asm ( ".section .dtors,\"aw\" "); /* here too a section change */
+static structor __DTOR_LIST__[1]={((structor)-1)};
+
+#endif
+
+__attribute__((section(".ctors")))
+static structor __CTOR_LIST__[1]={((structor)-1)};
+
+__attribute__((section(".dtors")))
+static structor __DTOR_LIST__[1]={((structor)-1)};
+
+static void __do_global_dtors_aux(void)
+{
+  structor *df=__CTOR_LIST__;	/* ugly trick to prevent warning */
+  for (df=((__DTOR_LIST__)+1);(*df) != (structor)0; df++) (*df)();
+}
+
+__attribute__((section(".fini"))) void _fini(void)
+{
+  __do_global_dtors_aux();
+}
+
+int _dyn_start(structor dl_init, int argc, char **argv, char **envp)
+{
+  static __attribute__((section(".init"))) void _init(void);
+  int main(int argc, char **argv, char **envp);
+
+  if (dl_init) atexit(dl_init);
+  _init();
+  atexit(_fini);
+  return main(argc, argv, envp);
+}
--- lib/atexit.c
+++ lib/atexit.c
@@ -1,10 +1,12 @@
 typedef void (*function)(void);
 
-static function __atexitlist[4];
+#define NUM_ATEXIT	8
+
+static function __atexitlist[NUM_ATEXIT];
 
 int atexit(function t) {
   int i;
-  for (i=0; i<4; i++)
+  for (i=0; i<NUM_ATEXIT; i++)
     if (__atexitlist[i]==0) {
       __atexitlist[i]=t;
       return 0;
@@ -15,9 +17,10 @@ int atexit(function t) {
 extern void _exit(int code) __attribute__((noreturn));
 
 void exit(int code) {
-  if (__atexitlist[3]) __atexitlist[3]();
-  if (__atexitlist[2]) __atexitlist[2]();
-  if (__atexitlist[1]) __atexitlist[1]();
-  if (__atexitlist[0]) __atexitlist[0]();
+  int i;
+  for (i=NUM_ATEXIT;i;) {
+    if (__atexitlist[--i])
+      __atexitlist[i]();
+  }
   _exit(code);
 }
