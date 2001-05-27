69ca3cef7958441e3b3ce6f40c3f7568395caed2
fefe <fefe>
2001-05-27 20:22:47 +0000

    add empty endhostent()

--- lib/nop.c
+++ lib/nop.c
@@ -5,3 +5,5 @@ int __fflush_stderr()	 __attribute__((weak,alias("__return0")));
 
 /* used for weak aliases */
 int __return0() { return 0; }
+
+void endhostent(void)	 __attribute__((weak,alias("__return0")));
