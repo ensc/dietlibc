603d955c7a92b5b6e07ae07aa555c63a407088be
fefe <fefe>
2001-05-31 18:13:10 +0000

    atol did not skip leading spaces, too :-(

--- lib/atol.c
+++ lib/atol.c
@@ -1,6 +1,7 @@
 long int atol(const char* s) {
   long int v=0;
   int sign=1;
+  while (isspace(*s)) ++s;
   if (*s=='-') { sign=-1; ++s; } else if (*s=='+') ++s;
   while (*s && (*s>='0') && (*s<='9'))
     v=v*10+*s++-'0';
