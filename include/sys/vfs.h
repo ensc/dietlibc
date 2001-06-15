568b31e1332600256e0c50e1d426e4fc7a89e9be
fefe <fefe>
2001-06-15 19:05:17 +0000

    remove kernel header dependency.

--- include/sys/vfs.h
+++ include/sys/vfs.h
@@ -3,7 +3,23 @@
 
 #include <sys/cdefs.h>
 #include <sys/types.h>
-#include <linux/vfs.h>
+
+typedef struct {
+	int     __val[2];
+} __kernel_fsid_t;
+
+struct statfs {
+	long f_type;
+	long f_bsize;
+	long f_blocks;
+	long f_bfree;
+	long f_bavail;
+	long f_files;
+	long f_ffree;
+	__kernel_fsid_t f_fsid;
+	long f_namelen;
+	long f_spare[6];
+};
 
 int statfs(const char *path, struct statfs *buf) __THROW;
 int fstatfs(int fd, struct statfs *buf) __THROW;
