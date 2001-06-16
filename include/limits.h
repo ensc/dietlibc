9be122c7ddf0589a6991a4789d90122298de4f56
fefe <fefe>
2001-06-16 16:17:18 +0000

    cleanse limits.h

--- include/limits.h
+++ include/limits.h
@@ -2,11 +2,6 @@
 #define _LIMITS_H
 
 #include <endian.h>
-#include <linux/limits.h>
-
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
 
 #define CHAR_BIT 8
 
@@ -37,6 +32,20 @@
 /* Maximum value an `unsigned long long int' can hold.  (Minimum is 0.)  */
 #define ULLONG_MAX 18446744073709551615ULL
 
-#define PASS_MAX 256
+#define PASS_MAX	256
 
+#define NR_OPEN		1024
+
+#define NGROUPS_MAX	32	/* supplemental group IDs are available */
+#define ARG_MAX		131072	/* # bytes of args + environ for exec() */
+#define CHILD_MAX	999    /* no limit :-) */
+#define OPEN_MAX	256	/* # open files a process may have */
+#define LINK_MAX	127	/* # links a file may have */
+#define MAX_CANON	255	/* size of the canonical input queue */
+#define MAX_INPUT	255	/* size of the type-ahead buffer */
+#define NAME_MAX	255	/* # chars in a file name */
+#define PATH_MAX	4095	/* # chars in a path name */
+#define PIPE_BUF	4096	/* # bytes in atomic write to a pipe */
+
+#define RTSIG_MAX	32
 #endif
