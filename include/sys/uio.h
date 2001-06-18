6d201c26b2fa2f649d4d05532aea04d0090f1c10
fefe <fefe>
2001-06-18 21:53:52 +0000

    don't get iovec from kernel.

--- include/sys/uio.h
+++ include/sys/uio.h
@@ -1,7 +1,7 @@
 #ifndef _SYS_UIO
 #define _SYS_UIO 1
 
-#include <linux/uio.h>
+#include <sys/socket.h>
 
 int readv(int filedes, const struct iovec *vector, size_t count);
 int writev(int filedes, const struct iovec *vector, size_t count);
