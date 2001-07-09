22c4799c176ba7f6c4625552c4259355aebb3edc
fefe <fefe>
2001-07-09 14:20:58 +0000

    if fd #0,#1 or #2 were already closed, daemon() could have closed one of them instead of connecting them all to /dev/null.

--- libugly/daemon.c
+++ libugly/daemon.c
@@ -8,28 +8,22 @@
 
 int daemon (int nochdir,int noclose)
 {
-   int fd;
-   switch (fork ())
-         {
-          case -1:
-                return (-1);
-          case 0:
-                break;
-          default:
-                _exit (0);
-         }
-   if (setsid () == -1)
-         return (-1);
-   if (!nochdir) chdir ("/");
-   if (!noclose)
-         {
-                fd = open (_PATH_DEVNULL,O_RDWR,0);
-                if (fd == -1) return (-1);
-                dup2 (fd,STDIN_FILENO);
-                dup2 (fd,STDOUT_FILENO);
-                dup2 (fd,STDERR_FILENO);
-                close (fd);
-         }
-   return (0);
+  int fd;
+  switch (fork()) {
+  case -1: return (-1);
+  case  0: break;
+  default: _exit (0);
+  }
+  if (setsid () == -1) return (-1);
+  if (!nochdir) chdir ("/");
+  if (!noclose) {
+    fd = open(_PATH_DEVNULL,O_RDWR,0);
+    if (fd == -1) return (-1);
+    dup2 (fd,STDIN_FILENO);
+    dup2 (fd,STDOUT_FILENO);
+    dup2 (fd,STDERR_FILENO);
+    if (fd>2) close (fd);
+  }
+  return (0);
 }
 
