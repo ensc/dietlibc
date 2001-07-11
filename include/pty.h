adb77319a9e48c730fb85e5d435cf0936bfd6b61
fefe <fefe>
2001-07-11 16:43:28 +0000

    add include/pty.h (declares openpty)

---
+++ include/pty.h
@@ -0,0 +1,13 @@
+#ifndef _PTY_H
+#define _PTY_H
+
+#include <sys/cdefs.h>
+#include <termios.h>
+
+/* Create pseudo tty master slave pair with NAME and set terminal
+ *    attributes according to TERMP and WINP and return handles for both
+ *       ends in AMASTER and ASLAVE.  */
+extern int openpty (int *__amaster, int *__aslave, char *__name, struct
+		    termios *__termp, struct winsize *__winp) __THROW;
+
+#endif
