762d01f52f52ed0ca9a2fd6cf8fdbcdfd2bfbc90
fefe <fefe>
2001-07-19 16:07:34 +0000

    oops, forgot to cvs add these

---
+++ lib/atoll.c
@@ -0,0 +1,18 @@
+#include <endian.h>
+#include <ctype.h>
+
+#if __WORDSIZE != 64
+long long int atol(const char* s) {
+  long long int v=0;
+  int sign=1;
+  while (isspace(*s)) ++s;
+  switch (*s) {
+  case '-': sign=-1;
+  case '+': ++s;
+  }
+  while ((unsigned int) (*s - '0') < 10u) {
+    v=v*10+*s-'0'; ++s;
+  }
+  return sign==-1?-v:v;
+}
+#endif
