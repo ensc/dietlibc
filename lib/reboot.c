a5a8dbc5438049974746b70eef1cdbb13ab63abe
fefe <fefe>
2001-07-19 15:40:56 +0000

    make reboot.c compile again and remove code alignment for i386.

--- lib/reboot.c
+++ lib/reboot.c
@@ -1,8 +1,8 @@
 #include <sys/reboot.h>
 
-int __reboot(int magic1, int magic2, int cmd, void * arg);
+int __reboot(int magic1, int magic2, int cmd);
 
-int reboot(int cmd, void *arg)
+int reboot(int cmd)
 {
-  return __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, cmd, arg);
+  return __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, cmd);
 }
