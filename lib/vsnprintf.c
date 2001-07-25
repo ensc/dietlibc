49c0500fc86e22734f86654500767ee13898920c
olaf <olaf>
2001-07-25 13:21:46 +0000

    Ups missing \0 termination ....

--- lib/vsnprintf.c
+++ lib/vsnprintf.c
@@ -17,12 +17,13 @@ static int swrite(void*ptr, size_t allways1, size_t nmemb, struct str_data* sd) 
     if (len>tmp) len=tmp;
     memcpy(&sd->str[sd->len],ptr,len);
     sd->len+=len;
+    sd->str[sd->len]=0;
   }
   return nmemb;
 }
 
 int vsnprintf(char* str, size_t size, const char *format, va_list arg_ptr) {
-  struct str_data sd = { str, 0, size };
+  struct str_data sd = { str, 0, size-1 };
   struct arg_printf ap = { &sd, (int(*)(void*,size_t,size_t,void*)) swrite };
   return __v_printf(&ap,format,arg_ptr);
 }
