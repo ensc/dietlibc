36d5aecbdbf53897674f323b8ff575a96d7da47b
olaf <olaf>
2001-08-20 13:14:26 +0000

    R_{386,ARM}_COPY relocation type...

--- libdl/_dl_relocate.c
+++ libdl/_dl_relocate.c
@@ -34,8 +34,9 @@ int _dl_apply_relocate(struct _dl_handle* dh, Elf32_Rel *rel) {
   if (typ==R_386_32) {			/* 1 */
     *loc = (unsigned long)(dh->mem_base+dh->dyn_sym_tab[ELF32_R_SYM(rel->r_info)].st_value);
   } else if (typ==R_386_COPY)  {	/* 5 */
+    int len=dh->dyn_sym_tab[ELF32_R_SYM(rel->r_info)].st_size;
     DEBUG("_dl_apply_relocate: R_386_COPY !\n");
-    ret=1;
+    memcpy(loc,(dh->mem_base+dh->dyn_sym_tab[ELF32_R_SYM(rel->r_info)].st_value),len);
   } else if (typ==R_386_GLOB_DAT) {	/* 6 */
     *loc = (unsigned long)_dl_sym(dh, ELF32_R_SYM(rel->r_info));
   } else if (typ==R_386_JMP_SLOT) {	/* 7 */
@@ -50,8 +51,9 @@ int _dl_apply_relocate(struct _dl_handle* dh, Elf32_Rel *rel) {
   if (typ==R_ARM_ABS32) {		/*  2 */
     *loc = (unsigned long)(dh->mem_base+dh->dyn_sym_tab[ELF32_R_SYM(rel->r_info)].st_value);
   } else if (typ==R_ARM_COPY)  {	/* 20 */
-    DEBUG("_dl_apply_relocate: R_386_COPY !\n");
-    ret=1;
+    int len=dh->dyn_sym_tab[ELF32_R_SYM(rel->r_info)].st_size;
+    DEBUG("_dl_apply_relocate: R_ARM_COPY !\n");
+    memcpy(loc,(dh->mem_base+dh->dyn_sym_tab[ELF32_R_SYM(rel->r_info)].st_value),len);
   } else if (typ==R_ARM_GLOB_DAT) {	/* 21 */
     *loc = (unsigned long)_dl_sym(dh, ELF32_R_SYM(rel->r_info));
   } else if (typ==R_ARM_JUMP_SLOT) {	/* 22 */
