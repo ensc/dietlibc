500e81b9ab0bcbe272f14bcf7986fae4e9afdde4
fefe <fefe>
2001-08-23 16:00:09 +0000

    add several more x86 assembly functions to shave off a few bytes here and there.

--- t.c
+++ t.c
@@ -86,7 +86,9 @@ void foo(int tmp,...) {
 }
 
 int main(int argc,char *argv[]) {
-  printf("%d\n",strncmp("aaaa","aaac",4));
+  char buf[20]="fnord";
+  printf("%s\n",strchr("gcc",'/'));
+  printf("%d\n",strcmp(buf,"fnord"));
 #if 0
   regex_t r;
 //  printf("regcomp %d\n",regcomp(&r,"^(re([\\[0-9\\]+])*|aw):[ \t]*",REG_EXTENDED));
