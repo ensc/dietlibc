6bd07184b97b83a7f0439a7d09253e75122795a8
olaf <olaf>
2001-10-04 13:46:25 +0000

    release the evil ! new alloc/free code

--- lib/alloc.c
+++ lib/alloc.c
@@ -1,15 +1,26 @@
 /*
  * malloc/free by O.Dreesen
+ *
+ * first TRY:
+ *   lists w/magics
+ * and now the second TRY
+ *   let the kernel map all the stuff (if there is something to do)
  */
 
 #include <unistd.h>
 #include <sys/mman.h>
 #include <errno.h>
 #include "dietfeatures.h"
+
+#include <sys/cdefs.h>
 #include <sys/types.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 #include <sys/shm.h>	/* for PAGE_SIZE */
+
+
+/* -- HELPER CODE --------------------------------------------------------- */
 
 #ifndef MAP_FAILED
 #define MAP_FAILED ((void*)-1)
@@ -19,343 +30,185 @@
 #define NULL ((void*)0)
 #endif
 
-extern void *memset(void *s, int c, size_t n);
-extern void *memcpy(void *dest, const void *src, size_t n);
+typedef union {
+  void*  next;
+  size_t size;
+} __alloc_t;
 
-typedef struct t_alloc_head {
-  unsigned long magic;
-  unsigned long size;
-} alloc_head;
+#define BLOCK_START(b)	(((void*)(b))-sizeof(__alloc_t))
+#define BLOCK_RET(b)	(((void*)(b))+sizeof(__alloc_t))
 
-typedef struct t_free_head {
-  unsigned long magic;
-  unsigned long size;
-  struct t_free_head *next;
-  struct t_free_head *prev;
-} free_head;
-
-/* guess what ? the virtual block size (this is the common page size) */
 #define MEM_BLOCK_SIZE	PAGE_SIZE
-
-/* minimum allocated bytes */
-#define MEM_ALLOC_MIN	(sizeof(free_head))
-
-/* what's the time to use mmu based mallocs ? def.: >64K(32bit) */
-#define USE_BIG_MALLOC	(16*MEM_BLOCK_SIZE)
-
-/* Initial start position in memory */
-#define MEM_ALLOC_START	((char*)0x18000000)
-
-/* Make every block align */
 #define PAGE_ALIGN(s)	(((s)+MEM_BLOCK_SIZE-1)&(unsigned long)(~(MEM_BLOCK_SIZE-1)))
-#define PAGE_ALIGNP(p)	((char*)PAGE_ALIGN((size_t)(p)))
-
-#define END_OF_BLOCK(p)	((free_head*)(((char*)(p))+((p)->size)))
-#define START_BLOCK(p)	((alloc_head*)(((char*)(p))-sizeof(alloc_head)))
-#define START_DATA(p)	(((char*)(p))+sizeof(alloc_head))
-#define MIN_ALLOC(s)	(((((s)+sizeof(alloc_head)-1)/MEM_ALLOC_MIN)+1)*MEM_ALLOC_MIN)
-
-#define FREE_MAGIC	0xdead2bad
-#define ALLOC_MAGIC	0xf82dead1
-#define BIG_MAGIC	0x2b162f81
 
 /* a simple mmap :) */
-static inline void *do_mmap(void *addr, unsigned long size)
-{
-  register int flags = MAP_ANONYMOUS|MAP_PRIVATE;
-  if (addr) flags|=MAP_FIXED;
-  return mmap(addr, size, PROT_READ|PROT_WRITE, flags, -1, 0);
+__attribute__((regparm(1)))
+static void *do_mmap(unsigned long size) {
+  return mmap(0, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
 }
 
-/* malloc BIG chunks / let the mmu make this for us ... */
-static void __big_free(void *ptr)
-{
-  alloc_head *ah;
-  if (ptr==NULL) return;
-  ah=START_BLOCK(ptr);
-  if (ah->magic!=BIG_MAGIC) return;
-  munmap(ah,ah->size);
-}
+/* -- SMALL MEM ----------------------------------------------------------- */
 
-static void *__big_malloc(size_t size)
-{
-  alloc_head *ah;
-  size+=sizeof(alloc_head);
-  size=PAGE_ALIGN(size);
-  ah=do_mmap(0, size);
-  if (ah==MAP_FAILED)
-  {
-    errno = ENOMEM;
-    return NULL;	/* PANIC ! */
+static __alloc_t* __small_mem[8];
+
+#define __SMALL_NR(i)		(MEM_BLOCK_SIZE/(i))
+
+#define __MIN_SMALL_SIZE	__SMALL_NR(256)		/*   16 /   32 */
+#define __MAX_SMALL_SIZE	__SMALL_NR(2)		/* 2048 / 4096 */
+
+#define GET_SIZE(s)		(__MIN_SMALL_SIZE<<get_index((s)))
+
+#define FIRST_SMALL(p)		(((unsigned long)(p))&(~(MEM_BLOCK_SIZE-1)))
+
+static inline int __ind_shift() { return (MEM_BLOCK_SIZE==4096)?4:5; }
+
+__attribute__((regparm(1)))
+static size_t get_index(size_t _size) {
+  register size_t idx=0;
+  if (_size) {
+    register size_t size=((_size-1)&(MEM_BLOCK_SIZE-1))>>__ind_shift();
+    while(size) { size>>=1; ++idx; }
   }
-  ah->magic	= BIG_MAGIC;
-  ah->size	= size;
-  return START_DATA(ah);
+  return idx;
 }
 
-static void *__big_realloc(void *ptr, size_t size)
-{
-  alloc_head *ah=START_BLOCK(ptr);
-  size+=sizeof(alloc_head);
-  size=PAGE_ALIGN(size);
-  if (ah->size!=size) {
-    if ((ah=mremap(ah,ah->size,size,MREMAP_MAYMOVE))==MAP_FAILED)
-      return 0;
-    ah->size=size;
+/* small mem */
+__attribute__((regparm(2)))
+static void __small_free(void*_ptr,size_t _size) {
+  __alloc_t* ptr=BLOCK_START(_ptr);
+  size_t size=_size;
+  size_t idx=get_index(size);
+
+  memset(ptr,0,size);	/* allways zero out small mem */
+
+  ptr->next=__small_mem[idx];
+  __small_mem[idx]=ptr;
+}
+
+__attribute__((regparm(1)))
+static void* __small_malloc(size_t _size) {
+  __alloc_t *ptr;
+  size_t size=_size;
+  size_t idx;
+
+  idx=get_index(size);
+  ptr=__small_mem[idx];
+
+  if (ptr==0)  {	/* no free blocks ? */
+    register int i,nr;
+    ptr=do_mmap(MEM_BLOCK_SIZE);
+    if (ptr==MAP_FAILED) return MAP_FAILED;
+
+    __small_mem[idx]=ptr;
+
+    nr=__SMALL_NR(size)-1;
+    for (i=0;i<nr;i++) {
+      ptr->next=(((void*)ptr)+size);
+      ptr=ptr->next;
+    }
+    ptr->next=0;
+
+    ptr=__small_mem[idx];
   }
-  return START_DATA(ah);
+
+  /* get a free block */
+  __small_mem[idx]=ptr->next;
+  ptr->next=0;
+
+  return ptr;
 }
 
-/* freelist handler */
-static free_head *base;
+/* -- PUBLIC FUNCTIONS ---------------------------------------------------- */
 
-static char *alloc_get_end = MEM_ALLOC_START;
-
-#ifdef DEBUG
-static void run_list()
-{
-  free_head *tmp;
-  printf("base: %08x\n",base);
-  for (tmp=base;tmp;tmp=tmp->next)
-  {
-    printf("%08x: %08x %08x %d\n",tmp,tmp->prev,tmp->next,tmp->size);
-  }
-}
-#endif
-
-void __libc_free(void *ptr);
-void __libc_free(void *ptr)
-{
-  register alloc_head *t;
-  free_head *next,*tmp,*block;
-
-  if (ptr==NULL) return;
-
-  t=START_BLOCK(ptr);
-  if (t->magic == BIG_MAGIC) { __big_free(ptr); return; }
-  if (t->magic != ALLOC_MAGIC) return;
-
-  block=(free_head*)t;
-  block->magic=FREE_MAGIC;
-  block->next=block->prev=0;
-
-  if (base)
-  {
-    if (block<base)
-    { /* pre freelist */
-      block->next=base;
-      tmp=base=block;
-    }
-    else
-    { /* in freelist or after last freelist-entry */
-      for (tmp=base; tmp->next ;tmp=tmp->next)
-      {
-	if (block<tmp->next) break;	/* FOUND */
-      }
-      block->next=tmp->next;
-      block->prev=tmp;
-      tmp->next=block;
-    }
-
-    next=block->next;
-    /* join code */
-    if (END_OF_BLOCK(block)==next)
-    { /* join right neighbor */
-      block->size+= next->size;
-      next=next->next;
-    }
-    block->next = next;
-    if (next) next->prev = block;
-
-    if (END_OF_BLOCK(tmp)==block)
-    { /* join left neighbor */
-      tmp->size += block->size;
-      tmp->next  = block->next;
-      if (next) next->prev = tmp;
+static void _alloc_libc_free(void *ptr) {
+  register size_t size;
+  if (ptr) {
+    size=((__alloc_t*)BLOCK_START(ptr))->size;
+    if (size) {
+      if (size<=__MAX_SMALL_SIZE)
+	__small_free(ptr,size);
+      else
+	munmap(BLOCK_START(ptr),size);
     }
   }
-  else
-  {
-    base=block; /* start freelist */
-  }
-#ifdef DEBUG
-  run_list();
-#endif
 }
-void free(void *ptr) __attribute__((weak,alias("__libc_free")));
+void __libc_free(void *ptr) __attribute__((alias("_alloc_libc_free")));
+void free(void *ptr) __attribute__((weak,alias("_alloc_libc_free")));
 
-static void *alloc_get_mem(unsigned long size)
-{
-  char *tmp;
-  alloc_head *ah;
 
-  size=PAGE_ALIGN(size);
-
-  /* map free pages @ alloc_get_end */
-  tmp=do_mmap(alloc_get_end, size);
-  if (tmp==MAP_FAILED)
-  {
-    /* OK we can't map free pages @ alloc_get_end so try free position */
-    tmp=do_mmap(0, size);
-    if (tmp==MAP_FAILED)
-    {
-      errno = ENOMEM;
-      return NULL;	/* PANIC ! */
-    }
-    alloc_get_end=tmp;
-  }
-
-  alloc_get_end+=size;
-
-  /* make a header */
-  ah=(alloc_head*)tmp;
-  ah->magic=ALLOC_MAGIC;
-  ah->size=size;
-
-  /* link new free maped pages in freelist */
-  __libc_free(START_DATA(tmp));
-
-  return base;
-}
-
-void *__libc_malloc(size_t size);
-void *__libc_malloc(size_t size)
-{
-  free_head *tmp;
+static void* _alloc_libc_malloc(size_t size) {
+  __alloc_t* ptr;
   size_t need;
-
-  /* needed MEM_ALLOC_MIN */
-  need=MIN_ALLOC(size);
-
-  if (size>USE_BIG_MALLOC) { return __big_malloc(size); }
-
-  if (base==NULL)
-  {
-    if (alloc_get_mem(need)==NULL) goto err_out;
-  }
-  tmp=base;
-  while(1)
-  {
-    if (tmp->size>=need)
-    {
-      /* relink freelist */
-      if (tmp==base) base=base->next;
-      if (tmp->prev) tmp->prev->next=tmp->next;
-      if (tmp->next) tmp->next->prev=tmp->prev;
-
-      if (tmp->size!=need)
-      { /* fit is not perfect */
-	alloc_head *tofree=(alloc_head*)(((char*)tmp)+need);
-	tofree->magic=ALLOC_MAGIC;
-	tofree->size=tmp->size-need;
-	free(START_DATA(tofree));
-	tmp->size=need;
-      }
-      tmp->magic=ALLOC_MAGIC;
-      return (void*)START_DATA(tmp);
+  if (size) {
+    size+=sizeof(__alloc_t);
+    if (size<=__MAX_SMALL_SIZE) {
+      need=GET_SIZE(size);
+      if ((ptr=__small_malloc(need))==MAP_FAILED) goto err_out;
     }
-    else if (tmp->next==NULL)
-    {
-      if (alloc_get_mem(need)==NULL) goto err_out;
-      tmp=base;
+    else {
+      need=PAGE_ALIGN(size);
+      if ((ptr=do_mmap(need))==MAP_FAILED) goto err_out;
     }
-    else
-      tmp=tmp->next;
+    ptr->size=need;
+    return BLOCK_RET(ptr);
   }
 err_out:
-  return NULL;
+  (*__errno_location())=ENOMEM;
+  return 0;
 }
-void *malloc(size_t size) __attribute__((weak,alias("__libc_malloc")));
+void* __libc_malloc(size_t size) __attribute__((alias("_alloc_libc_malloc")));
+void* malloc(size_t size) __attribute__((weak,alias("_alloc_libc_malloc")));
 
-void *calloc(size_t nmemb,size_t size)
-{
-  size_t n=nmemb*size;
-  void *tmp=malloc(n);
-  if (tmp) memset(tmp,0,n);
-  return tmp;
+void *calloc(size_t nmemb, size_t _size) {
+  register size_t size=_size*nmemb;
+  return malloc(size);
 }
 
-void *realloc(void *ptr,size_t size)
-{
-  alloc_head *tmp=0;
-  free_head *tf=0;
-  long need=0;
-  long diff=0;
-
-  if (ptr)
-  {
-    if (size)
-    {
-      tmp=START_BLOCK(ptr);
-      if (tmp->magic == BIG_MAGIC) return __big_realloc(ptr,size);
-
-      need=MIN_ALLOC(size);  /* only this size will survive */
-      diff=tmp->size-need;
-      if (diff<0)
-      {
-	int in_free=0;
-	tf=END_OF_BLOCK(tmp);
-	{
-	  free_head* tmp;
-	  for (tmp=base; tmp->next ;tmp=tmp->next)
-	    if (tmp==tf) {
-	      in_free=1;
-	      break;
-	    }
-	}
-	if ((in_free)&&(tf->magic==FREE_MAGIC)&&(need<USE_BIG_MALLOC))
-	{
-	  if ((tf->size<(unsigned long)(-diff))&&(tf->next==NULL))
-	  { /* next ist last block ? */
-	    alloc_get_mem((unsigned long)need);
+void* __libc_realloc(void* ptr, size_t _size);
+void* __libc_realloc(void* ptr, size_t _size) {
+  register size_t size=_size;
+  if (ptr) {
+    if (size) {
+      __alloc_t* tmp=BLOCK_START(ptr);
+      size+=sizeof(__alloc_t);
+      size=(size<=__MAX_SMALL_SIZE)?GET_SIZE(size):PAGE_ALIGN(size);
+      if (tmp->size!=size) {
+	if ((tmp->size<=__MAX_SMALL_SIZE)) {
+	  void *new=_alloc_libc_malloc(_size);
+	  if (new) {
+	    register __alloc_t* foo=BLOCK_START(new);
+	    size=foo->size;
+	    if (size>tmp->size) size=tmp->size;
+	    memcpy(new,ptr,size-sizeof(__alloc_t));
+	    _alloc_libc_free(ptr);
 	  }
-	  if (tf->size>(unsigned long)(-diff))
-	  {
-	    free_head *f=(free_head*)(((char*)tmp)+need);
-#ifdef DEBUG
-	    printf("found free block to realloc: %08x %08x %d %08x %d %08x %d\n",ptr,tmp,tmp->size,tf,tf->size,f,tf->size+diff);
-	    printf("new: %08x %d\n",tmp,need);
-	    run_list();
-#endif
-	    /* delink */
-	    if (tf==base) base=tf->next;
-	    if (tf->prev) tf->prev->next = tf->next;
-	    if (tf->next) tf->next->prev = tf->prev;
-
-	    if (tf->size+diff)
-	    { /* free unused part */
-	      f->magic=ALLOC_MAGIC;
-	      f->size=tf->size+diff;
-	      free(START_DATA(f));
-	    }
-
-	    tmp->size=need;
-	    return (void*)START_DATA(tmp);
+	  ptr=new;
+	}
+	else {
+	  register __alloc_t* foo;
+	  size=PAGE_ALIGN(size);
+	  foo=mremap(tmp,tmp->size,size,MREMAP_MAYMOVE);
+	  if (foo==MAP_FAILED) {
+	    (*__errno_location())=ENOMEM;
+	    ptr=0;
+	  }
+	  else {
+	    foo->size=size;
+	    ptr=BLOCK_RET(foo);
 	  }
 	}
-	if ((tf=malloc(size)))
-	{
-	  memcpy(tf,ptr,tmp->size-sizeof(alloc_head));
-	  free(ptr);
-	  return tf;
-	}
-	return NULL;
       }
-      if (diff>=(long)sizeof(alloc_head))
-      {
-	tmp->size=need;
-	tf=END_OF_BLOCK(tmp);
-	tf->magic=ALLOC_MAGIC;
-	tf->next=tf;
-	tf->size=diff;
-	free(START_DATA(tf));
-      }
-      return ptr;
     }
-    else
-      free(ptr);
+    else { /* size==0 */
+      _alloc_libc_free(ptr);
+    }
   }
-  else if (size>0)
-    return malloc(size);
-  return NULL;
+  else { /* ptr==0 */
+    if (size) {
+      ptr=_alloc_libc_malloc(size);
+    }
+  }
+  return ptr;
 }
+void* realloc(void* ptr, size_t size) __attribute__((weak,alias("__libc_realloc")));
+
--- libpthread/pthread_sys_alloc.c
+++ libpthread/pthread_sys_alloc.c
@@ -7,8 +7,7 @@
 
 static pthread_mutex_t mutex_alloc = PTHREAD_MUTEX_INITIALIZER;
 
-void free(void *ptr)
-{
+void free(void *ptr) {
   __NO_ASYNC_CANCEL_BEGIN;
   pthread_mutex_lock(&mutex_alloc);
   __libc_free(ptr);
@@ -16,12 +15,21 @@ void free(void *ptr)
   __NO_ASYNC_CANCEL_END;
 }
 
-void *malloc(size_t size)
-{
+void *malloc(size_t size) {
   register void *ret;
   __NO_ASYNC_CANCEL_BEGIN;
   pthread_mutex_lock(&mutex_alloc);
   ret=__libc_malloc(size);
+  pthread_mutex_unlock(&mutex_alloc);
+  __NO_ASYNC_CANCEL_END;
+  return ret;
+}
+
+void* realloc(void* ptr, size_t size) { /* the code is not thread-save */
+  register void *ret;
+  __NO_ASYNC_CANCEL_BEGIN;
+  pthread_mutex_lock(&mutex_alloc);
+  __libc_realloc(ptr, size);
   pthread_mutex_unlock(&mutex_alloc);
   __NO_ASYNC_CANCEL_END;
   return ret;
