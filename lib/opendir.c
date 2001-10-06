dcc3c08ba664eadc003320415aaabc150ce0a1ed
fefe <fefe>
2001-10-06 03:15:16 +0000

    opendir did not close-on-exec :-(

--- lib/opendir.c
+++ lib/opendir.c
@@ -4,17 +4,23 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-DIR*  opendir ( const char* name )
-{
-    int   fd = open (name, O_RDONLY | O_DIRECTORY);
-    DIR*  t  = NULL;
+DIR*  opendir ( const char* name ) {
+  int   fd = open (name, O_RDONLY | O_DIRECTORY);
+  DIR*  t  = NULL;
 
-    if ( fd >= 0 ) {
-        t = (DIR*) calloc ((sizeof(DIR)+15)/16, 16);
-        if (t == NULL)
-            close (fd);
-        else
-            t->fd = fd;
+  if ( fd >= 0 ) {
+    if (fcntl (fd, F_SETFD, FD_CLOEXEC) < 0) {
+      t=0;
+      goto lose;
     }
-    return t;
+    t = (DIR*) calloc ((sizeof(DIR)+15)/16, 16);
+    if (t == NULL)
+lose:
+      close (fd);
+    else
+      t->fd = fd;
+  }
+
+
+  return t;
 }
