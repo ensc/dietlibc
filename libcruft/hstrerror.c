b2423e401c337b6488910a2e3dccd4dbd33bf6cb
fefe <fefe>
2001-10-06 23:38:16 +0000

    hstrerror default error message "host not found"

--- libcruft/hstrerror.c
+++ libcruft/hstrerror.c
@@ -3,8 +3,8 @@
 const char* hstrerror(int h_errno) {
   switch (h_errno) {
   case 0: return "OK";
-  case HOST_NOT_FOUND: return "Unknown host";
   case TRY_AGAIN: return "Temporary failure.";
-  default: return "DNS error (hehe)";
+  case HOST_NOT_FOUND:
+  default: return "Unknown host";
   }
 }
