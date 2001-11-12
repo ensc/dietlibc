16a831e6efdcd18cda05469ef438ebffc028be77
fefe <fefe>
2001-11-12 23:43:44 +0000

    duh, off-by-one return code in the face of ungetc (that function should be taken behind the barn and shot)

--- libstdio/fread.c
+++ libstdio/fread.c
@@ -6,17 +6,18 @@ size_t fread( void *ptr, size_t size, size_t nmemb, FILE *stream) {
   int res;
   unsigned long i,j;
   j=size*nmemb;
+  i=0;
 
   if (!j) return 0;
   if (stream->ungotten) {
     stream->ungotten=0;
     *(char*)ptr=stream->ungetbuf;
-    ptr=((char*)ptr)+1;
     --j;
+    ++i;
   }
   if (!j) return 1;
 
-  for (i=0; i<j; ++i) {
+  for (; i<j; ++i) {
     res=fgetc(stream);
     if (res==EOF)
       return i/size;
