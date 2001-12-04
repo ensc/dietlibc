2e43a94cb0cab6c0f113e1bbdad569f7961282bd
leitner <leitner>
2001-12-04 14:38:13 +0000

    remove the offending __kernel_fsid_t.

--- include/asm/types.h
+++ include/asm/types.h
@@ -29,12 +29,4 @@ typedef unsigned long __kernel_size_t;
 typedef unsigned int __kernel_size_t;
 #endif
 
-typedef struct {
-#if defined(__KERNEL__) || defined(__USE_ALL)
-	int	val[2];
-#else /* !defined(__KERNEL__) && !defined(__USE_ALL) */
-	int	__val[2];
-#endif /* !defined(__KERNEL__) && !defined(__USE_ALL) */
-} __kernel_fsid_t;
-
 #endif
