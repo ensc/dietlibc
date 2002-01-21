27efe64f0e8ada46013f233165fa7db9ffcf835b
fefe <fefe>
2002-01-21 16:52:57 +0000

    free memory in setenv in case of error

--- libugly/setenv.c
+++ libugly/setenv.c
@@ -7,7 +7,7 @@ link_warning("setenv","setenv calls malloc.  Avoid it in small programs.");
 int setenv(const char *name, const char *value, int overwrite) {
   char *c=malloc(strlen(name)+strlen(value)+3);
   if (getenv(name)) {
-    if (!overwrite) return 0;
+    if (!overwrite) { free(c); return 0; }
     unsetenv(name);
   }
   strcpy(c,name);
