42a131f321b5dc242edf22124c6b48e7c74ad992
fefe <fefe>
2002-01-24 00:43:10 +0000

    make setenv smaller (Thomas Ogrisegg)

--- libugly/setenv.c
+++ libugly/setenv.c
@@ -5,13 +5,15 @@
 link_warning("setenv","setenv calls malloc.  Avoid it in small programs.");
 
 int setenv(const char *name, const char *value, int overwrite) {
-  char *c=malloc(strlen(name)+strlen(value)+3);
   if (getenv(name)) {
-    if (!overwrite) { free(c); return 0; }
+    if (!overwrite) return 0;
     unsetenv(name);
   }
-  strcpy(c,name);
-  strcat(c,"=");
-  strcat(c,value);
-  return putenv(c);
+  {
+    char *c=malloc(strlen(name)+strlen(value)+3);
+    strcpy(c,name);
+    strcat(c,"=");
+    strcat(c,value);
+    return putenv(c);
+  }
 }
