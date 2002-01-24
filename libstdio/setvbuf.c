9a955cb6a98f2b48efaf2c035150cdfee87e098e
fefe <fefe>
2002-01-24 00:27:57 +0000

    setvbuf fix for size==0

--- libstdio/setvbuf.c
+++ libstdio/setvbuf.c
@@ -4,25 +4,31 @@
 #include <stdlib.h>
 #include "dietwarning.h"
 
-int setvbuf(FILE *stream, char *buf, int flags , size_t size) {
-  stream->flags|=STATICBUF;
+static int set_flags (FILE *stream, int flags) {
+  switch (flags) {
+    case _IONBF: stream->flags = (stream->flags & ~(BUFLINEWISE)) | NOBUF; break;
+    case _IOLBF: stream->flags = (stream->flags & ~(NOBUF)) | BUFLINEWISE; break;
+    case _IOFBF: stream->flags = stream->flags & ~(NOBUF | BUFLINEWISE); break;
+    default: return -1;
+  }
+  return 0;
+}
+
+int setvbuf(FILE *stream, char *buf, int flags, size_t size) {
   if (buf) {
     if (!(stream->flags&STATICBUF)) free(stream->buf);
     stream->buf=buf;
   }
   else {
     char *tmp;
+    if (!size) {
+      return set_flags(stream,flags);
+    }
     if (!(tmp=malloc(size))) return -1;
     if (!(stream->flags&STATICBUF)) free(stream->buf);
     stream->buf=tmp;
   }
   stream->buflen=size;
   stream->bm=stream->bs=0;
-  switch (flags) {
-  case _IONBF: stream->flags = (stream->flags & ~(BUFLINEWISE)) | NOBUF; break;
-  case _IOLBF: stream->flags = (stream->flags & ~(BUFLINEWISE|NOBUF)) | BUFLINEWISE; break;
-  case _IOFBF: stream->flags = stream->flags & ~(NOBUF | BUFLINEWISE); break;
-  default: return -1;
-  }
-  return 0;
+  return set_flags(stream,flags);
 }
