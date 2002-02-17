e63fe4466f455ef52ee986466656587b17fb2f56
fefe <fefe>
2002-02-17 23:53:03 +0000

    add strstr test suite and repair bug unearthed by it.

--- lib/strstr.c
+++ lib/strstr.c
@@ -5,9 +5,11 @@ char *strstr(const char *haystack, const char *needle) {
   size_t nl=strlen(needle);
   size_t hl=strlen(haystack);
   int i;
+  if (!nl) goto found;
   if (nl>hl) return 0;
   for (i=hl-nl+1; i; --i) {
     if (*haystack==*needle && !memcmp(haystack,needle,nl))
+found:
       return (char*)haystack;
     ++haystack;
   }
---
+++ test/strstr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <assert.h>
+
+main() {
+  char buf[]="foo bar baz";
+  assert(strstr(buf,"bar")==buf+4);
+  assert(strstr(buf,"baz")==buf+8);
+  assert(strstr(buf,"barney")==0);
+  assert(strstr(buf,"foo")==buf);
+  assert(strstr(buf,"")==buf);
+}
