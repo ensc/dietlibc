687c163735c497adb8bd99fde4c4c1aca69f7f1a
fefe <fefe>
2002-02-23 20:26:45 +0000

    more cosmetic changes, provide a prototype for usleep

--- lib/usleep.c
+++ lib/usleep.c
@@ -3,9 +3,9 @@
 /* nano * 1000 == usecs
  * usecs * 1000 == msecs
  * msecs * 1000 = secs */
-void usleep(unsigned int usecs) {
+int usleep(unsigned long usecs) {
   struct timespec t;
   t.tv_sec=usecs/1000000;
   t.tv_nsec=(usecs%1000000)*1000;
-  nanosleep(&t,&t);
+  return nanosleep(&t,&t);
 }
