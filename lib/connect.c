9c6a00c677c46a2e3b42a5d9b567cb02b5ef49a8
fefe <fefe>
2002-02-23 20:24:11 +0000

    cosmetic typo fix

--- lib/connect.c
+++ lib/connect.c
@@ -7,8 +7,8 @@ int __libc_connect(int a);
 int __libc_connect(int a) {
   return socketcall(SYS_CONNECT, (long*)&a);
 #else
-int __libc_connect(int a, void * b, int c) {
 int __libc_connect(int a, void * b, int c);
+int __libc_connect(int a, void * b, int c) {
   unsigned long args[] = { a, (long) b, c };
   return socketcall(SYS_CONNECT, args);
 #endif
