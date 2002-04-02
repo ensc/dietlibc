b5c3510386550000b44efdb7519e4f9cabd587d1
fefe <fefe>
2002-04-02 16:06:15 +0000

    remove broken assignment

--- profiling/monitor.c
+++ profiling/monitor.c
@@ -29,7 +29,7 @@ monitor (long lowpc, long highpc)
 	mparam.highpc     = highpc;
 	mparam.lowpc      = lowpc;
 	mparam.kcountsize = (mparam.textsize = highpc-lowpc) >> 1;
-	mparam.kcount = (u_short *) malloc (mparam.kcountsize = highpc-lowpc);
+	mparam.kcount = (u_short *) malloc (mparam.textsize);
 	mparam.arcs = (struct rawarc *) malloc (MAXARCS*sizeof (struct rawarc));
 	if (!mparam.kcount || !mparam.arcs)
 		exit (42);
