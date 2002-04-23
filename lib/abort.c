0b39a0637c116c998e4e25bf5122392ac0adbf42
fefe <fefe>
2002-04-23 13:35:41 +0000

    abort() did not flush all streams

--- lib/abort.c
+++ lib/abort.c
@@ -1,9 +1,11 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void abort() {
   sigset_t t;
+  fflush(0);
   if (!sigemptyset(&t) && !sigaddset(&t, SIGABRT))
     sigprocmask(SIG_UNBLOCK, &t, 0);
   while (1)
