c106d2badcab996afb01d42cd11e69efcc45110f
fefe <fefe>
2002-04-29 19:23:15 +0000

    better assert-based memchr

--- test/memchr.c
+++ test/memchr.c
@@ -1,9 +1,11 @@
 #include <string.h>
+#include <assert.h>
 
 int main() {
   const char* test="blubber";
-  if (memchr("aaaa",'x',4)) return 1;
-  if (memchr(0,'x',0)) return 1;
-  if (memchr(test,'u',7) != test+2) return 1;
+  assert(memchr("aaaa",'x',4)==0);
+  assert(memchr(0,'x',0)==0);
+  assert(memchr(test,'u',7) == test+2);
+  assert(memchr(test,'b',7)==test);
   return 0;
 }
