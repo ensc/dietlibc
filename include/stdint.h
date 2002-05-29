01bd6b6fcce350ce4e85e6e7f9a6c0fff1831581
fefe <fefe>
2002-05-29 13:23:34 +0000

    make 64 bit types conditional

--- include/stdint.h
+++ include/stdint.h
@@ -6,11 +6,14 @@
 typedef int8_t int_least8_t;
 typedef int16_t int_least16_t;
 typedef int32_t int_least32_t;
-typedef int64_t int_least64_t;
 typedef uint8_t uint_least8_t;
 typedef uint16_t uint_least16_t;
 typedef uint32_t uint_least32_t;
+
+#ifndef __STRICT_ANSI__
+typedef int64_t int_least64_t;
 typedef uint64_t uint_least64_t;
+#endif
 
 typedef signed char int_fast8_t;
 typedef signed long int int_fast16_t;
