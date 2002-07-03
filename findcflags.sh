fba4774842636acf6f8fc6b8da44c820ae24db2a
fefe <fefe>
2002-07-03 14:07:58 +0000

    auto-detect gcc 3.1 on x86 and remove compiler warnings

---
+++ findcflags.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+version=`${1:-gcc} -v 2>&1 |grep version|sed 's/gcc version //'`
+case $version in
+  2.9*) echo -march=i386 -Os -fomit-frame-pointer -malign-functions=0 -malign-jumps=0 -malign-loops=0 -mpreferred-stack-boundary=2 ;;
+  3.0*) echo -march=i386 -Os -fomit-frame-pointer -malign-functions=0 -malign-jumps=0 -malign-loops=0 -mpreferred-stack-boundary=2 ;;
+  3*) echo -Os -fomit-frame-pointer -falign-functions=0 -falign-jumps=0 -falign-loops=0 -mpreferred-stack-boundary=2;;
+  *) echo -O2 -pipe -fomit-frame-pointer ;;
+esac
