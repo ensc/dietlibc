5777fbb415d087231e2c681dcc84939ded59e6cc
fefe <fefe>
2002-08-21 21:46:38 +0000

    improve findcflags for gcc -v output of recent versions

--- findcflags.sh
+++ findcflags.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=`${1:-gcc} -v 2>&1 |grep version|sed 's/gcc version //'`
+version=`${1:-gcc} -v 2>&1 |sed -n '/gcc version/ {s/gcc version //;p}'`
 case $version in
   2.9*) echo -march=i386 -Os -fomit-frame-pointer -malign-functions=0 -malign-jumps=0 -malign-loops=0 -mpreferred-stack-boundary=2 ;;
   3.0*) echo -march=i386 -Os -fomit-frame-pointer -malign-functions=0 -malign-jumps=0 -malign-loops=0 -mpreferred-stack-boundary=2 ;;
