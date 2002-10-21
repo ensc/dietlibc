9761657335bb285d9aa995e1569f6f20357f1d0b
fefe <fefe>
2002-10-21 13:02:06 +0000

    reversed implementations

--- lib/__ftruncate64.c
+++ lib/__ftruncate64.c
@@ -3,17 +3,18 @@
 #ifdef WANT_LARGEFILE_BACKCOMPAT
 #include <sys/stat.h>
 #include "syscalls.h"
+#include <unistd.h>
 #ifndef __NO_STAT64
 #ifdef __NR_ftruncate64
 
-extern int __dietlibc_ftruncate64(const char *f, loff_t o);
+extern int __dietlibc_ftruncate64(int fd, loff_t o);
 
-int ftruncate64(const char *f, loff_t o) {
+int ftruncate64(int fd, loff_t o) {
   int tmp;
-  if ((tmp=__dietlibc_ftruncate64(f,o))==-1) {
+  if ((tmp=__dietlibc_ftruncate64(fd,o))==-1) {
     if (errno!=ENOSYS) return -1;
     if (o>0x7fffffff) { errno=EOVERFLOW; return -1; }
-    return ftruncate(f,o);
+    return ftruncate(fd,o);
   }
   return tmp;
 }
--- lib/__truncate64.c
+++ lib/__truncate64.c
@@ -3,17 +3,18 @@
 #ifdef WANT_LARGEFILE_BACKCOMPAT
 #include <sys/stat.h>
 #include "syscalls.h"
+#include <unistd.h>
 #ifndef __NO_STAT64
 #ifdef __NR_truncate64
 
-extern int __dietlibc_truncate64(int fd, loff_t o);
+extern int __dietlibc_truncate64(const char* f, loff_t o);
 
-int truncate64(int fd, loff_t o) {
+int truncate64(const char* f, loff_t o) {
   int tmp;
-  if ((tmp=__dietlibc_truncate64(fd,o))==-1) {
+  if ((tmp=__dietlibc_truncate64(f,o))==-1) {
     if (errno!=ENOSYS) return -1;
     if (o>0x7fffffff) { errno=EOVERFLOW; return -1; }
-    return truncate(fd,o);
+    return truncate(f,o);
   }
   return tmp;
 }
