9b95005fb42abb451af3accd908dd7fc9029489a
fefe <fefe>
2002-11-03 00:14:35 +0000

    damn!  My QA failed again, the i386 strcasecmp was broken and broke ssh

--- test/strcasecmp.c
+++ test/strcasecmp.c
@@ -10,8 +10,9 @@ void die(const char* message) {
 main() {
   char buf[100]="fNord";
   char buf2[100]="fnOrt";
-  if (strcasecmp(buf,buf)) die("strcmp say a != a");
+  if (strcasecmp(buf,buf)) die("strcmp sais a != a");
   if (strcasecmp(buf,buf2)>=0) die("strcmp said fnord > fnort");
   if (strcasecmp(buf2,buf)<=0) die("strcmp said fnort < fnord");
+  if (strcasecmp("Host","hostbasedauthentication")==0) die("strcmp said a == abc");
   return 0;
 }
