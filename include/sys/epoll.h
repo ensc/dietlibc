daa5887638066f7ec6aaa2e75cf137e4759bb409
fefe <fefe>
2002-11-24 19:24:47 +0000

    change the API back to the "final" API. http://www.xmailserver.org/linux-patches/sys_epoll-2.5.49-0.58.diff

--- include/sys/epoll.h
+++ include/sys/epoll.h
@@ -10,9 +10,21 @@
 #define EPOLL_CTL_DEL 2	/* Remove a file decriptor from the interface */
 #define EPOLL_CTL_MOD 3	/* Change file decriptor epoll_event structure */
 
+typedef union epoll_data {
+  void *ptr;
+  int fd;
+  __uint32_t u32;
+  __uint64_t u64;
+} epoll_data_t;
+
+struct epoll_event {
+  uint32_t events;
+  epoll_data_t data;
+};
+
 int epoll_create(int size) __THROW;
-int epoll_ctl(int epfd, int op, int fd, unsigned int events) __THROW;
-int epoll_wait(int epfd, struct pollfd *events, int maxevents,
+int epoll_ctl(int epfd, int op, int fd, struct epoll_event* event) __THROW;
+int epoll_wait(int epfd, struct epoll_event *events, int maxevents,
 	       int timeout) __THROW;
 
 #endif
