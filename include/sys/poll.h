d7bc5a2b877913ffe04fb8abc86a67b5b0118102
fefe <fefe>
2003-01-04 22:21:47 +0000

    ia64 and x86_64 don't use "ipc" syscall

--- include/sys/poll.h
+++ include/sys/poll.h
@@ -3,19 +3,32 @@
 
 #include <sys/cdefs.h>
 
-#define POLLIN		0x0001
-#define POLLPRI		0x0002
-#define POLLOUT		0x0004
-#define POLLERR		0x0008
-#define POLLHUP		0x0010
-#define POLLNVAL	0x0020
-#define POLLRDNORM	0x0040
-#define POLLRDBAND	0x0080
-#define POLLWRBAND	0x0200
-#define POLLMSG		0x0400
+enum {
+  POLLIN	= 0x0001,
+#define POLLIN		POLLIN
+  POLLPRI	= 0x0002,
+#define POLLPRI		POLLPRI
+  POLLOUT	= 0x0004,
+#define POLLOUT		POLLOUT
+  POLLERR	= 0x0008,
+#define POLLERR		POLLERR
+  POLLHUP	= 0x0010,
+#define POLLHUP		POLLHUP
+  POLLNVAL	= 0x0020,
+#define POLLNVAL	POLLNVAL
+  POLLRDNORM	= 0x0040,
+#define POLLRDNORM	POLLRDNORM
+  POLLRDBAND	= 0x0080,
+#define POLLRDBAND	POLLRDBAND
+  POLLWRBAND	= 0x0200,
+#define POLLWRBAND	POLLWRBAND
+  POLLMSG	= 0x0400,
+#define POLLMSG		POLLMSG
 /* POLLREMOVE is for /dev/epoll (/dev/misc/eventpoll),
  * a new event notification mechanism for 2.6 */
-#define POLLREMOVE	0x1000
+  POLLREMOVE	= 0x1000,
+#define POLLREMOVE	POLLREMOVE
+};
 
 #if defined(__sparc__) || defined (__mips__)
 #define POLLWRNORM	POLLOUT
@@ -30,6 +43,5 @@ struct pollfd {
 };
 
 extern int poll(struct pollfd *ufds, unsigned int nfds, int timeout) __THROW;
-
 
 #endif	/* _SYS_POLL_H */
