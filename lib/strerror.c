296fa01f845e03bc5418b97537371f35080e950c
olaf <olaf>
2003-01-13 15:16:28 +0000

    added system specific errlists for alpha,mips,sparc and sparc64

--- lib/strerror.c
+++ lib/strerror.c
@@ -5,13 +5,16 @@
 #define _BSD_SOURCE
 #include <errno.h>
 
-extern const char  __sys_err_unknown [];
+extern const char __sys_err_unknown[];
 
-char* strerror ( int errnum )
-{
-    register const char*  message = __sys_err_unknown;
+char*strerror(int errnum) {
+  register const char*message=__sys_err_unknown;
 
-    if ( (unsigned int)errnum < (unsigned int)__SYS_NERR )
-        message = sys_errlist [errnum];
-    return (char*)message;
+  if ( (unsigned int)errnum < (unsigned int)__SYS_NERR )
+    message=sys_errlist[errnum];
+#if defined(__mips__)
+  if ( errnum == 1133 )
+    message="Quota exceeded";
+#endif
+  return (char*)message;
 }
