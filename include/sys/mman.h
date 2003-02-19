0140479b5a8e658c553f91908b1a640ee4a5e0b4
olaf <olaf>
2003-02-19 20:07:48 +0000

    clean up of multiple definitions...

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -15,7 +15,7 @@
 #define MAP_SHARED	0x01		/* Share changes */
 #define MAP_PRIVATE	0x02		/* Changes are private */
 
-#ifdef __mips__
+#if defined(__mips__)
 #define MAP_FIXED	0x010		/* Interpret addr exactly */
 #define MAP_NORESERVE	0x0400		/* don't check for reservations */
 #define MAP_ANONYMOUS	0x0800		/* don't use a file */
@@ -33,9 +33,7 @@
 #define MADV_SEQUENTIAL	0x2		/* read-ahead aggressively */
 #define MADV_WILLNEED	0x3		/* pre-fault pages */
 #define MADV_DONTNEED	0x4		/* discard these pages */
-#else
-
-#ifdef __alpha__
+#elif defined(__alpha__)
 #define MAP_FIXED	0x100		/* Interpret addr exactly */
 #define MAP_ANONYMOUS	0x10		/* don't use a file */
 #define MAP_GROWSDOWN	0x1000		/* stack-like segment */
@@ -54,12 +52,9 @@
 #define MADV_WILLNEED	3		/* will need these pages */
 #define MADV_SPACEAVAIL	5		/* ensure resources are available */
 #define MADV_DONTNEED	6		/* dont need these pages */
-#else
+#elif defined(__i386__) || defined(__s390__) || defined(__x86_64__)
 #define MAP_FIXED	0x10		/* Interpret addr exactly */
 #define MAP_ANONYMOUS	0x20		/* don't use a file */
-#endif
-
-#if defined(__i386__) || defined(__s390__) || defined(__x86_64__)
 #define MAP_GROWSDOWN	0x0100		/* stack-like segment */
 #define MAP_DENYWRITE	0x0800		/* ETXTBSY */
 #define MAP_EXECUTABLE	0x1000		/* mark it as an executable */
@@ -76,19 +71,21 @@
 #define MADV_WILLNEED	0x3		/* pre-fault pages */
 #define MADV_DONTNEED	0x4		/* discard these pages */
 #elif defined(__sparc__) || defined (__powerpc__)
-#define MAP_RENAME      MAP_ANONYMOUS   /* In SunOS terminology */
-#define MAP_NORESERVE   0x40            /* don't reserve swap pages */
-#define MAP_INHERIT     0x80            /* SunOS doesn't do this, but... */
-#define MAP_LOCKED      0x100           /* lock the mapping */
-#define _MAP_NEW        0x80000000      /* Binary compatibility is fun... */
+#define MAP_FIXED	0x10		/* Interpret addr exactly */
+#define MAP_ANONYMOUS	0x20		/* don't use a file */
+#define MAP_RENAME	MAP_ANONYMOUS	/* In SunOS terminology */
+#define MAP_NORESERVE	0x40		/* don't reserve swap pages */
+#define MAP_INHERIT	0x80		/* SunOS doesn't do this, but... */
+#define MAP_LOCKED	0x100		/* lock the mapping */
+#define _MAP_NEW	0x80000000	/* Binary compatibility is fun... */
 #define MAP_GROWSDOWN	0x0100		/* stack-like segment */
 #define MAP_DENYWRITE	0x0800		/* ETXTBSY */
 #define MAP_EXECUTABLE	0x1000		/* mark it as an executable */
 #define MS_ASYNC	1		/* sync memory asynchronously */
 #define MS_INVALIDATE	2		/* invalidate the caches */
 #define MS_SYNC		4		/* synchronous memory sync */
-#define MCL_CURRENT     0x2000          /* lock all currently mapped pages */
-#define MCL_FUTURE      0x4000          /* lock all additions to address space */
+#define MCL_CURRENT	0x2000		/* lock all currently mapped pages */
+#define MCL_FUTURE	0x4000		/* lock all additions to address space */
 #define MADV_NORMAL	0x0		/* default page-in behavior */
 #define MADV_RANDOM	0x1		/* page-in minimum required */
 #define MADV_SEQUENTIAL	0x2		/* read-ahead aggressively */
@@ -96,6 +93,8 @@
 #define MADV_DONTNEED	0x4		/* discard these pages */
 #define MADV_FREE	0x5		/* (Solaris) contents can be freed */
 #elif defined (__arm__)
+#define MAP_FIXED	0x10		/* Interpret addr exactly */
+#define MAP_ANONYMOUS	0x20		/* don't use a file */
 #define MAP_GROWSDOWN	0x0100		/* stack-like segment */
 #define MAP_DENYWRITE	0x0800		/* ETXTBSY */
 #define MAP_EXECUTABLE	0x1000		/* mark it as an executable */
@@ -113,43 +112,41 @@
 #define MADV_DONTNEED	0x4		/* discard these pages */
 
 #elif defined(__hppa__)
-#define MAP_TYPE    0x03        /* Mask for type of mapping */
-#define MAP_FIXED   0x04        /* Interpret addr exactly */
-#define MAP_ANONYMOUS   0x10        /* don't use a file */
+#define MAP_TYPE	0x03	/* Mask for type of mapping */
+#define MAP_FIXED	0x04	/* Interpret addr exactly */
+#define MAP_ANONYMOUS	0x10	/* don't use a file */
 
-#define MAP_DENYWRITE   0x0800      /* ETXTBSY */
-#define MAP_EXECUTABLE  0x1000      /* mark it as an executable */
-#define MAP_LOCKED  0x2000      /* pages are locked */
-#define MAP_NORESERVE   0x4000      /* don't check for reservations */
-#define MAP_GROWSDOWN   0x8000      /* stack-like segment */
+#define MAP_DENYWRITE	0x0800	/* ETXTBSY */
+#define MAP_EXECUTABLE	0x1000	/* mark it as an executable */
+#define MAP_LOCKED	0x2000	/* pages are locked */
+#define MAP_NORESERVE	0x4000	/* don't check for reservations */
+#define MAP_GROWSDOWN	0x8000	/* stack-like segment */
 
-#define MS_SYNC     1       /* synchronous memory sync */
-#define MS_ASYNC    2       /* sync memory asynchronously */
-#define MS_INVALIDATE   4       /* invalidate the caches */
+#define MS_SYNC 	1	/* synchronous memory sync */
+#define MS_ASYNC	2	/* sync memory asynchronously */
+#define MS_INVALIDATE	4	/* invalidate the caches */
 
-#define MCL_CURRENT 1       /* lock all current mappings */
-#define MCL_FUTURE  2
+#define MCL_CURRENT	1	/* lock all current mappings */
+#define MCL_FUTURE	2
 
 #elif defined(__ia64__)
 
-#define MAP_TYPE    0x0f        /* Mask for type of mapping */
-#define MAP_FIXED   0x10        /* Interpret addr exactly */
-#define MAP_ANONYMOUS   0x20        /* don't use a file */
+#define MAP_TYPE	0x0f	/* Mask for type of mapping */
+#define MAP_FIXED	0x10	/* Interpret addr exactly */
+#define MAP_ANONYMOUS	0x20	/* don't use a file */
 
-#define MAP_GROWSDOWN   0x0100      /* stack-like segment */
-#define MAP_GROWSUP 0x0200      /* register stack-like segment */
-#define MAP_DENYWRITE   0x0800      /* ETXTBSY */
-#define MAP_EXECUTABLE  0x1000      /* mark it as an executable */
-#define MAP_LOCKED  0x2000      /* pages are locked */
-#define MAP_NORESERVE   0x4000      /* don't check for reservations */
-#define MAP_WRITECOMBINED 0x10000   /* write-combine the area */
-#define MAP_NONCACHED   0x20000     /* don't cache the memory */
+#define MAP_GROWSDOWN	0x0100	/* stack-like segment */
+#define MAP_GROWSUP	0x0200	/* register stack-like segment */
+#define MAP_DENYWRITE	0x0800	/* ETXTBSY */
+#define MAP_EXECUTABLE	0x1000	/* mark it as an executable */
+#define MAP_LOCKED	0x2000	/* pages are locked */
+#define MAP_NORESERVE	0x4000	/* don't check for reservations */
+#define MAP_WRITECOMBINED 0x10000	/* write-combine the area */
+#define MAP_NONCACHED	0x20000	/* don't cache the memory */
 
-#define MS_ASYNC    1       /* sync memory asynchronously */
-#define MS_INVALIDATE   2       /* invalidate the caches */
-#define MS_SYNC     4       /* synchronous memory sync */
-
-#endif
+#define MS_ASYNC	1	/* sync memory asynchronously */
+#define MS_INVALIDATE	2	/* invalidate the caches */
+#define MS_SYNC 	4	/* synchronous memory sync */
 
 #endif
 
