b3862d206d71d7a1382e623820ababb333352ac7
fefe <fefe>
2003-03-14 16:42:28 +0000

    add more BSD brokenness to sys/time.h

--- include/sys/time.h
+++ include/sys/time.h
@@ -63,4 +63,13 @@ struct tm {
   const char *tm_zone;		/* Timezone abbreviation.  */
 };
 
+#ifdef _BSD_SOURCE
+/* another wonderful BSD invention... :( */
+/* note how subtly broken it is (doesn't work with <= or >=) */
+#define timercmp(a,b,CMP) (((a)->tv_sec == (b)->tv_sec) ? ((a)->tv_usec CMP (b)->tv_usec) : ((a)->tv_sec CMP (b)->tv_sec))
+#define timerclear(x) ((x)->tv_sec=(x)->tv_usec=0)
+#define timeradd(a,b,x) do { (x)->tv_sec=(a)->tv_sec+(b)->tv_sec; if (((x)->tv_usec=(a)->tv_usec+(b)->tv_usec)>=1000000) { ++(x)->tv_sec; (x)->tv_usec-=1000000; } } while (0)
+#define timersub(a,b,x) do { (x)->tv_sec=(a)->tv_sec-(b)->tv_sec; if (((x)->tv_usec=(a)->tv_usec-(b)->tv_usec)<0) { --(x)->tv_sec; (x)->tv_usec+=1000000; } } while (0)
+#endif
+
 #endif
