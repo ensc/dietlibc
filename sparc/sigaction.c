c1662baa782434b11a30aaa9b5a0d0282443620f
sanjiyan <sanjiyan>
2003-06-04 16:16:49 +0000

    missing __libc_sigaction symbol

--- sparc/sigaction.c
+++ sparc/sigaction.c
@@ -2,6 +2,10 @@
 
 int __rt_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact, void* restorer, long nr);
 
-int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) {
+int __libc_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+int __libc_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) {
   return __rt_sigaction(signum, act, oldact, 0, _NSIG/8);
 }
+int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact)
+__attribute__((weak,alias("__libc_sigaction")));
+
