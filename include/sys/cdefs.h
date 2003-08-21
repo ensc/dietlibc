57b3d7d321a512dc86d003ebd6bf1d18d5a5bc92
leitner <leitner>
2003-08-21 22:54:10 +0000

    oops, declared __BEGIN_DECLS twice

--- include/sys/cdefs.h
+++ include/sys/cdefs.h
@@ -63,12 +63,4 @@
 #define __deprecated__
 #endif
 
-#ifdef	__cplusplus
-# define __BEGIN_DECLS	extern "C" {
-# define __END_DECLS	}
-#else
-# define __BEGIN_DECLS
-# define __END_DECLS
-#endif
-
 #endif
