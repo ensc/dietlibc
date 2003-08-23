2441261a563d6c23079d87f6a5e1efe7e42a45b1
leitner <leitner>
2003-08-23 16:50:54 +0000

    remove warnings for #define of getline (it's no longer a #define)

--- libcompat/getline.c
+++ libcompat/getline.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include "dietwarning.h"
 
-#undef getline
 ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
   return getdelim(lineptr,n,'\n',stream);
 }
-link_warning("getline","warning: you used getline without include stdio.h w/_GNU_SOURCE")
