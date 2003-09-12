8fd9002878e910fc63bcc731e981f191a69feb86
leitner <leitner>
2003-09-12 12:33:13 +0000

    praise the busybox people (see CHANGES)

--- libstdio/fputc_unlocked.c
+++ libstdio/fputc_unlocked.c
@@ -3,22 +3,27 @@
 #include <endian.h>
 
 int fputc_unlocked(int c, FILE *stream) {
-  if (__fflush4(stream,0)) return EOF;
+  if (__fflush4(stream,0)) {
+kaputt:
+    stream->flags|=ERRORINDICATOR;
+    return EOF;
+  }
   if (stream->bm>=stream->buflen-1)
-    if (fflush(stream)) return EOF;
+    if (fflush(stream)) goto kaputt;
   if (stream->flags&NOBUF) {
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-    if (write(stream->fd,&c,1) != 1) return EOF;
+    if (write(stream->fd,&c,1) != 1)
 #else
-    if (write(stream->fd,(char*)&c+sizeof(c)-1,1) != 1) return EOF;
+    if (write(stream->fd,(char*)&c+sizeof(c)-1,1) != 1)
 #endif
+      goto kaputt;
     return 0;
   }
   stream->buf[stream->bm]=c;
   ++stream->bm;
   if (((stream->flags&BUFLINEWISE) && c=='\n') ||
       ((stream->flags&NOBUF))) /* puke */
-    if (fflush(stream)) return EOF;
+    if (fflush(stream)) goto kaputt;
   return 0;
 }
 
--- libstdio/freopen.c
+++ libstdio/freopen.c
@@ -1,9 +1,24 @@
 #include <sys/types.h>
 #include <dietstdio.h>
 #include <unistd.h>
+#include <errno.h>
 
+/* TODO:
+ * - check if mode is equel to the old mode
+ * - errorindicator test ?
+ */
 FILE *freopen (const char *path, const char *mode, FILE *stream) {
-  fclose(stream);
-  stream=fopen(path,mode);
+  if (stream) {
+    fflush(stream);
+    if (close(stream->fd)) stream=0;
+    else {
+      int f=__stdio_parse_mode(mode);
+      if ((stream->fd=open(path,f,0666))!=-1) {
+	struct stat st;
+	fstat(stream->fd,&st);
+	stream->flags=(S_ISFIFO(st.st_mode))?FDPIPE:0;
+      }
+    }
+  } else errno=EINVAL;
   return stream;
 }
