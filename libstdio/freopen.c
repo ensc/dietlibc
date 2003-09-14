edcfe833e444af56f0dc2ad19d4febbdfbc9ea84
leitner <leitner>
2003-09-14 23:21:27 +0000

    freopen should ignore it if close fails

--- libstdio/freopen.c
+++ libstdio/freopen.c
@@ -6,19 +6,17 @@
 
 FILE *freopen (const char *path, const char *mode, FILE *stream) {
   if (stream) {
+    int f=__stdio_parse_mode(mode);
     fflush(stream);
-    if (close(stream->fd)) stream=0;
-    else {
-      int f=__stdio_parse_mode(mode);
-      if ((stream->fd=open(path,f,0666))!=-1) {
-	struct stat st;
-	fstat(stream->fd,&st);
-	stream->flags=(S_ISFIFO(st.st_mode))?FDPIPE:0;
-	switch (f&3) {
-	case O_RDWR: stream->flags|=CANWRITE;
-	case O_RDONLY: stream->flags|=CANREAD; break;
-	case O_WRONLY: stream->flags|=CANWRITE;
-	}
+    close(stream->fd);
+    if ((stream->fd=open(path,f,0666))!=-1) {
+      struct stat st;
+      fstat(stream->fd,&st);
+      stream->flags=(S_ISFIFO(st.st_mode))?FDPIPE:0;
+      switch (f&3) {
+      case O_RDWR: stream->flags|=CANWRITE;
+      case O_RDONLY: stream->flags|=CANREAD; break;
+      case O_WRONLY: stream->flags|=CANWRITE;
       }
     }
   } else errno=EINVAL;
