4dfab218d7ff8886b937200c0537af0f2d0a1b2d
leitner <leitner>
2003-09-22 00:12:19 +0000

    EOVERFLOW handling in ftell()

--- libstdio/ftell.c
+++ libstdio/ftell.c
@@ -1,7 +1,19 @@
 #include <dietstdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 long ftell(FILE *stream) {
+  off_t l;
   fflush(stream);
-  return (lseek(stream->fd,0,SEEK_CUR)-stream->ungotten);
+  l=lseek(stream->fd,0,SEEK_CUR);
+  if (l==-1) return -1;
+  l-=stream->ungotten;
+  {
+    long x=l;
+    if (x!=l) {
+      errno=EOVERFLOW;
+      return -1;
+    }
+    return x;
+  }
 }
