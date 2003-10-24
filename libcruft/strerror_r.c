61a38d51fec35106608cbab5f393a398e7d492a0
leitner <leitner>
2003-10-24 15:21:50 +0000

    remove bashing comment in strerror_r.  Uwe Ohse points out that this function may actually make sense in locale scenarios.

--- libcruft/strerror_r.c
+++ libcruft/strerror_r.c
@@ -2,10 +2,6 @@
 
 extern const char __sys_err_unknown[];
 
-/* This function is _really_ useless, since strerror does not have a
- * static buffer or possibly conflicting data manipulations that could
- * be optimized away with this function.  What a crook.  Why oh why
- * doesn't susv3 define gethostbyname_r but does define this crap? */
 int strerror_r(int errnum, char *buf, size_t n) {
   const char* x=strerror(errnum);
   if (x==__sys_err_unknown || n<1) return -1;
