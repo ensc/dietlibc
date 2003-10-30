231d964409ceb2a95fe743120393bdf5df50132a
leitner <leitner>
2003-10-30 12:34:14 +0000

    Forgot to copy service name

--- libugly/getnameinfo.c
+++ libugly/getnameinfo.c
@@ -40,6 +40,8 @@ int getnameinfo(const struct sockaddr *sa, socklen_t salen, char *host,
       struct servent *s;
       if (!(s=getservbyport(port,flags&NI_DGRAM?"udp":"tcp")))
 	return EAI_SERVICE;
+      strncpy(serv,s.s_name,servlen-1);
+      serv[servlen-1]=0;
     }
   }
   return 0;
