d48b6f28e4d17e1518203adb4b7e4ecfb9fdcd8e
leitner <leitner>
2003-11-14 14:38:18 +0000

    locking and ungetc updates (Manuel Novoa III)

--- libstdio/ftello.c
+++ libstdio/ftello.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 
 off_t ftello_unlocked(FILE *stream) {
-  fflush(stream);
-  return (lseek(stream->fd,0,SEEK_CUR));
+  off_t l;
+  fflush_unlocked(stream);
+  return ((l=lseek(stream->fd,0,SEEK_CUR))==-1?-1:l-stream->ungotten);
 }
 
 off_t ftello(FILE *stream) __attribute__((weak,alias("ftello_unlocked")));
--- libstdio/ftello64.c
+++ libstdio/ftello64.c
@@ -3,8 +3,9 @@
 
 #ifndef __NO_STAT64
 off64_t ftello64_unlocked(FILE *stream) {
+  off64_t l;
   fflush_unlocked(stream);
-  return (lseek64(stream->fd,0ull,SEEK_CUR));
+  return ((l=lseek64(stream->fd,0ull,SEEK_CUR))==-1?-1:l-stream->ungotten);
 }
 
 off64_t ftello64(FILE *stream) __attribute__((weak,alias("ftello64_unlocked")));
