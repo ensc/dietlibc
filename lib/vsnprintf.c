a60c055362e219010b2a0d7c286080da677c4a6b
leitner <leitner>
2003-12-05 14:10:10 +0000

    a few sprintf fixes

--- lib/vsnprintf.c
+++ lib/vsnprintf.c
@@ -27,12 +27,11 @@ static int swrite(void*ptr, size_t nmemb, struct str_data* sd) {
 
 int vsnprintf(char* str, size_t size, const char *format, va_list arg_ptr) {
   long n;
-  struct str_data sd = { str, 0, size-1 };
+  struct str_data sd = { str, 0, size?size-1:0 };
   struct arg_printf ap = { &sd, (int(*)(void*,size_t,void*)) swrite };
-  if (size) --sd.size;
   n=__v_printf(&ap,format,arg_ptr);
   if (str) {
-    if (((long)n>(long)size) && size>0) str[size]=0;
+    if (size!=(size_t)-1 && ((long)n>(long)size) && size>0) str[size]=0;
     else str[n]=0;
   }
   return n;
