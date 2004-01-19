b5f8b1e8776d85acf1580c48d11453b495a2c405
leitner <leitner>
2004-01-19 16:47:23 +0000

    document the WANT_SYSENTER and user-mode-linux problem

--- dietfeatures.h
+++ dietfeatures.h
@@ -40,6 +40,7 @@
 
 /* on i386, Linux has an alternate syscall method since 2002/12/16 */
 /* on my Athlon XP, it is twice as fast, but it's only in kernel 2.5 */
+/* 20040118: enabling this breaks User Mode Linux!  It's their fault. */
 #define WANT_SYSENTER
 
 #define WANT_LINKER_WARNINGS
