f4364bb4b38a2d318a8c7be1f0ac487eb90054f8
leitner <leitner>
2004-05-06 14:49:41 +0000

    gcc 3.4 fix for non-x86

--- lib/write12.c
+++ lib/write12.c
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <write12.h>
 
-int __write1 (const char* s) {
+int __attribute__ (( regparm(1) )) __write1 (const char* s) {
   return write(1, s, strlen(s));
 }
 
-int __write2 (const char* s) {
+int __attribute__ (( regparm(1) )) __write2 (const char* s) {
   return write(2, s, strlen(s));
 }
