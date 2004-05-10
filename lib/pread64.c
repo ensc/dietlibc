95b39865fd13689d58507931a18c7aec153026df
leitner <leitner>
2004-05-10 13:03:34 +0000

    fix pread64

--- lib/pread64.c
+++ lib/pread64.c
@@ -3,11 +3,11 @@
 #include <sys/stat.h>
 
 #ifndef __NO_STAT64
-extern size_t __pread(int fd, void *buf, size_t count, off_t a,off_t b);
+extern size_t __pread(int fd, void *buf, size_t count, off64_t a);
 
 size_t __libc_pread64(int fd, void *buf, size_t count, off64_t offset);
 size_t __libc_pread64(int fd, void *buf, size_t count, off64_t offset) {
-  return __pread(fd,buf,count,__LONG_LONG_PAIR ((off_t)(offset&0xffffffff),(off_t)(offset>>32)));
+  return __pread(fd,buf,count,offset);
 }
 
 int pread64(int fd, void *buf, size_t count, off_t offset) __attribute__((weak,alias("__libc_pread64")));
