077de81a69ad420ce60a69cc9c9ac94554c68d35
leitner <leitner>
2004-07-05 21:50:21 +0000

    add some BSD legacy declarations to dirent.h (Bryan Henderson)

--- include/dirent.h
+++ include/dirent.h
@@ -63,6 +63,35 @@ extern int dirfd(DIR *dirp) __THROW __attribute_dontuse__;
 #define alphasort alphasort64
 #endif
 
+#ifdef _BSD_SOURCE
+/* File types for `d_type'.  */
+enum
+  {
+    DT_UNKNOWN = 0,
+# define DT_UNKNOWN	DT_UNKNOWN
+    DT_FIFO = 1,
+# define DT_FIFO	DT_FIFO
+    DT_CHR = 2,
+# define DT_CHR		DT_CHR
+    DT_DIR = 4,
+# define DT_DIR		DT_DIR
+    DT_BLK = 6,
+# define DT_BLK		DT_BLK
+    DT_REG = 8,
+# define DT_REG		DT_REG
+    DT_LNK = 10,
+# define DT_LNK		DT_LNK
+    DT_SOCK = 12,
+# define DT_SOCK	DT_SOCK
+    DT_WHT = 14
+# define DT_WHT		DT_WHT
+  };
+
+/* Convert between stat structure types and directory types.  */
+# define IFTODT(mode)	(((mode) & 0170000) >> 12)
+# define DTTOIF(dirtype)	((dirtype) << 12)
+#endif
+
 __END_DECLS
 
 #endif
