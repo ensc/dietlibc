290e4bd1dab8624538bcc45335668164216a6c41
leitner <leitner>
2005-08-05 17:59:45 +0000

    more rpc brokenness

--- librpc/svc_auth_unix.c
+++ librpc/svc_auth_unix.c
@@ -130,7 +130,9 @@ register struct rpc_msg *msg;
  * Shorthand unix authenticator
  * Looks up longhand in a cache.
  */
- /*ARGSUSED*/ static enum auth_stat _svcauth_short(rqst, msg)
+enum auth_stat _svcauth_short(struct svc_req* rqst, struct rpc_msg* msg);
+
+enum auth_stat _svcauth_short(rqst, msg)
 struct svc_req *rqst;
 struct rpc_msg *msg;
 {
