cb11efdf095058d02987b830fa5c347bb1062432
leitner <leitner>
2007-01-20 22:47:43 +0000

    fix putenv (removed also if prefix, not identity)

--- lib/putenv.c
+++ lib/putenv.c
@@ -15,9 +15,11 @@ int putenv(const char *string) {
     len=strlen(string);
     remove=1;
   } else
-    len=tmp-string+1;
+    len=tmp-string;
   for (envc=0, ep=(const char**)environ; (ep && *ep); ++ep) {
-    if (*string == **ep && !memcmp(string,*ep,len)) {
+    if (*string == **ep &&
+	!memcmp(string,*ep,len) &&
+	(*ep)[len]=='=') {
       if (remove) {
 	for (; ep[1]; ++ep) ep[0]=ep[1];
 	ep[0]=0;
