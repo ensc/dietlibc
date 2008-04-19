14ab2ce64a681b6ff64c007068dd816f6495dd13
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2008-04-19 17:39:23 +0200

    Fixes/enhancements for INF/NAN handling in printf()
    This patch adds support for uppercase 'F' and 'G' printf format
    specifiers.  It fixes handling of -INF values in __dtostr() too;
    previously, there was
    
    | unsigned int i;
    | if ((i=isinf(d))) return copystring(buf,maxlen,i>0?"inf":"-inf");
                                                     ~~~
    which evaluated to true everytime. The copystring() function
    worked for 3-letter words only but not for '-inf'.
    
    The last argument of __dtostr() was changed from a boolean flag to
    a bitmask. Bit 0 encodes 'g' or 'f', and bit 1 lower-/uppercase.
    There should be probably added some macros for them; for now,
    these values are used directly.
    
    Please note that this might affect other applications (liblowfat?)
    too which are using __dtostr().
    
    'isinf(3)' is a builtin with gcc 4.3 and does not give a hint
    about the signess of the infinity anymore. Hence, this patch uses
    a more portable way where needed.

--- include/stdlib.h
+++ include/stdlib.h
@@ -28,8 +28,12 @@ long double strtold(const char *nptr, char **endptr) __THROW;
 long int strtol(const char *nptr, char **endptr, int base) __THROW;
 unsigned long int strtoul(const char *nptr, char **endptr, int base) __THROW;
 
+/* HACK: used flags in __dtostr
+     0x01 ... 'g'
+     0x02 ... uppercase
+   Define some constants somewhere... */
 extern int __ltostr(char *s, unsigned int size, unsigned long i, unsigned int base, int UpCase) __THROW;
-extern int __dtostr(double d,char *buf,unsigned int maxlen,unsigned int prec,unsigned int prec2,int g) __THROW;
+extern int __dtostr(double d,char *buf,unsigned int maxlen,unsigned int prec,unsigned int prec2,int flags) __THROW;
 
 #if !defined(__STRICT_ANSI__) || __STDC_VERSION__ + 0 >= 199900L
 __extension__ long long int strtoll(const char *nptr, char **endptr, int base) __THROW;
