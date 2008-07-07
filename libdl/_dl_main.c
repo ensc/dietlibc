c33520fab318264414611e440b33b44c129462ac
sanjiyan <sanjiyan>
2008-07-07 12:31:53 +0000

    more patches for dynamic ARM from Gernot Tenchio.

--- libdl/_dl_main.c
+++ libdl/_dl_main.c
@@ -318,6 +318,7 @@ asm(".text \n"
 "	mov	fp, #0			@ start new stack frame \n"
 "	ldr	a1, [sp], #4		@ argc \n"
 "	mov	a2, sp			@ argv \n"
+"	mov	sp, r4			@ restore stack pointer \n"
 "	add	a3, a2, a1, lsl #2	@ envp \n"
 "	add	a3, a3, #4 \n"
 /* PIC code startup */
