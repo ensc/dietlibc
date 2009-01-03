03ce135c609f1c68f22bba38d45a7b34732e5045
Benedikt Böhm <bb@xnull.de>
2009-01-03 17:17:22 +0100

    define socketcall wth PIC too

--- i386/syscalls.h
+++ i386/syscalls.h
@@ -368,7 +368,6 @@ sym: \
 .Lend##sym: ; \
 .size sym,.Lend##sym-sym
 
-#ifndef __PIC__
 #define __socketcall(name,NAME) \
 .text; \
 .type name,@function; \
@@ -381,6 +380,3 @@ __libc_##name: ; \
 	jmp socketcall; \
 .Lend##name:; \
 .size name,.Lend##name-name
-#else
-#define __socketcall(name,NAME)
-#endif
