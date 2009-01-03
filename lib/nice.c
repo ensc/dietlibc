ef7b3f0619235fd1b78c60d84077c4c32b5e617f
Benedikt Böhm <bb@xnull.de>
2009-01-03 16:40:54 +0100

    implement a sane nice based on getpriority and setpriority (POSIX requires nice to return the new nice value, but the syscall doesn't, so we use {set,get}priority and implement nice in libc)

--- lib/nice.c
+++ lib/nice.c
@@ -1,20 +1,56 @@
-#ifndef _REENTRANT
-#define _REENTRANT
-#endif
+/*
+ * nice() for uClibc
+ *
+ * Copyright (C) 2000-2004 by Erik Andersen <andersen@codepoet.org>
+ * Copyright (C) 2005 by Manuel Novoa III <mjn3@codepoet.org>
+ *
+ * GNU Library General Public License (LGPL) version 2 or later.
+ */
+
 #include <errno.h>
-#include <unistd.h>
+#include <limits.h>
 #include <sys/resource.h>
 
-int nice(int incr) {
-  int prio;
-  int res;
-  errno=0;
-  prio = getpriority(PRIO_PROCESS,0) + incr;
-  if (prio < PRIO_MIN) prio=PRIO_MIN;
-  if (prio >= PRIO_MAX) prio=PRIO_MAX-1;
-  if (setpriority (PRIO_PROCESS, 0, prio)==-1) {
-    if (errno==EACCES) errno=EPERM;
-    return -1;
-  } else
-    return getpriority(PRIO_PROCESS, 0);
+static inline int int_add_no_wrap(int a, int b)
+{
+	int	s;
+
+	if ((b > 0) && (a > (INT_MAX - b)))
+		s = INT_MAX;
+	else if ((b < 0) && (a < (INT_MIN - b)))
+		s = INT_MIN;
+	else
+		s = a + b;
+
+	return s;
+}
+
+static inline int __syscall_nice(int incr)
+{
+	int old_priority;
+	int old_errno;
+
+	old_errno = errno;
+	__set_errno(0);
+	old_priority = getpriority(PRIO_PROCESS, 0);
+	if ((old_priority == -1) && errno) {
+		return -1;
+	}
+	__set_errno(old_errno);
+
+	if (setpriority(PRIO_PROCESS, 0, int_add_no_wrap(old_priority, incr))) {
+		__set_errno(EPERM);	/* SUSv3 mandates EPERM for nice failure. */
+		return -1;
+	}
+
+	return 0;
+}
+
+int nice(int incr)
+{
+	if (__syscall_nice(incr)) {
+		return -1;
+	}
+
+	return getpriority(PRIO_PROCESS, 0);
 }
