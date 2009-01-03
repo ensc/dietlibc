56bf672289b123a4a4d56e0977b098771904891f
Benedikt Böhm <bb@xnull.de>
2009-01-03 16:56:33 +0100

    remove bogus tcsetattr shortcut

--- lib/tcsetattr.c
+++ lib/tcsetattr.c
@@ -3,24 +3,8 @@
 #include <errno.h>
 #include "dietfeatures.h"
 
-#if !defined(__powerpc__) && !defined(__sparc__) && !defined(__alpha__) && !defined(__hppa__)
-#if TCSANOW==0 && TCSADRAIN==1 && TCSAFLUSH==2 && TCSETSW-TCSETS==1 && TCSETSF-TCSETS==2
-#define shortcut
-#endif
-#endif
-
 int  tcsetattr ( int fildes, int optional_actions, struct termios* termios_p )
 {
-#ifdef shortcut
-
-    if ( (unsigned int)optional_actions < 3u )
-        return ioctl ( fildes, TCSETS+optional_actions, termios_p );
-
-    errno = EINVAL;
-    return -1;
-
-#else
-
     switch ( optional_actions ) {
     case TCSANOW:
         return ioctl ( fildes, TCSETS , termios_p );
@@ -32,6 +16,4 @@ int  tcsetattr ( int fildes, int optional_actions, struct termios* termios_p )
         errno = EINVAL;
         return -1;
     }
-    
-#endif    
 }
