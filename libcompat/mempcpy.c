09639f9a8714f692e611ac333237990825eaa43e
leitner <leitner>
2009-05-30 06:15:09 +0000

    remove warning

--- libcompat/mempcpy.c
+++ libcompat/mempcpy.c
@@ -1,6 +1,9 @@
+#define _GNU_SOURCE
 #include <string.h>
 
-char *mempcpy(char *dst, const char *src, size_t n) {
+void *mempcpy(void* __restrict__ _dst, const void* __restrict__ _src, size_t n) {
+  char* dst=_dst;
+  const char* src=_src;
   size_t i;
   for (i=0; i<n; ++i)
     dst[i]=src[i];
