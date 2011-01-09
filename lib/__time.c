34b923987e653c35e5a5acb80d147706b6a27d36
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2011-01-09 14:20:56 +0100

    __time.c: fixed typo

--- lib/__time.c
+++ lib/__time.c
@@ -7,7 +7,7 @@ time_t time(time_t *t)
 {
   struct timeval tv;
   if (__unlikely(gettimeofday(&tv, NULL) < 0))
-    tv.v_sec = -1;
+    tv.tv_sec = -1;
   if (t)
     *t = tv.tv_sec;
   return tv.tv_sec;
