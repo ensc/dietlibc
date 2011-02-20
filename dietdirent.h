568b42ca261b72573f2b61b671e608b7b082cfcc
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2011-02-20 20:06:28 +0100

    enforce 64-bit alignment of dirstream buffer

--- dietdirent.h
+++ dietdirent.h
@@ -6,7 +6,7 @@ struct __dirstream {
   int fd;
   unsigned int num;
   unsigned int cur;
-  char buf[];
+  char buf[] __attribute__((__aligned__(8)));
 };				/* stream data from opendir() */
 
 #define __DIRSTREAM_BUF_SIZE	(__DIET_PAGE_SIZE - offsetof(struct __dirstream, buf))
