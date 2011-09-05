db6d15f919fb0e95c77bdb38850873d5f2ff6a2e
leitner <leitner>
2011-09-05 00:07:45 +0000

    memchr on i386 was going one byte too far

--- test/memchr.c
+++ test/memchr.c
@@ -5,6 +5,7 @@ int main() {
   const char* test="blubber";
   assert(memchr("aaaa",'x',4)==0);
   assert(memchr(0,'x',0)==0);
+  assert(memchr(test,'u',2) == NULL);
   assert(memchr(test,'u',7) == test+2);
   assert(memchr(test,'b',7)==test);
   assert(memchr(test+6,'r',1)==test+6);
