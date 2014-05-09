8400f89d04bf354e337892cd3f6de1cd4e906efd
leitner <leitner>
2014-05-09 17:10:10 +0000

    make assertion nicer

--- lib/reallocarray.c
+++ lib/reallocarray.c
@@ -2,8 +2,7 @@
 
 void* reallocarray(void* ptr, size_t nmemb, size_t size) {
   /* produce a compile-time error if uintmax_t is not larger than size_t */
-  char dummy[(sizeof(uintmax_t)>=sizeof(size_t)*2)-1];
-  (void)dummy;
+  typedef char compile_time_assertion[(sizeof(uintmax_t)>=sizeof(size_t)*2)-1];
 
   uintmax_t l=(uintmax_t)nmemb * size;
   if ((size_t)l != l) return 0;	// overflow
