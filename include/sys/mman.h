e9e4f6733335f1029bcbadceb625c54f631701b7
Alex Suykov <alex.suykov@gmail.com>
2014-12-31 17:28:46 +0200

    mremap proto to match mremap(2) and uClibc/glibc headers

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -267,7 +267,7 @@ extern int munmap (void *__addr, size_t __len) __THROW;
 extern int mprotect (void *__addr, size_t __len, int __prot) __THROW;
 extern int msync (void *__addr, size_t __len, int __flags) __THROW;
 extern void *mremap (void *__addr, size_t __old_len, size_t __new_len,
-		     unsigned long __may_move) __THROW;
+		     int __may_move, ...) __THROW;
 extern int mincore (void *__start, size_t __len, unsigned char *__vec);
 
 extern void *mmap64 (void *__addr, size_t __len, int __prot,
