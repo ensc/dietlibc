748137ef30cd20fadf06e9ea8c2503bd625cedfb
leitner <leitner>
2015-04-11 04:13:29 +0000

    explicit non-executable stack marking for some x64 functions add x64 asm memcmp, memset, memcpy remove untested warning from x64 clone

--- test/memcmp.c
+++ test/memcmp.c
@@ -5,6 +5,9 @@ int main() {
   const char* test="blubber";
   assert(memcmp(test,"blubber",8)==0);
   assert(memcmp(test,"fnord",5)<0);
+  assert(memcmp(test,"armenien",8)>0);
+  /* now check if the endianness conversion works right */
+  assert(memcmp("bbbacbbb","bbbbbbbb",8)<0);
   assert(memcmp(test,0,0)==0);
   return 0;
 }
