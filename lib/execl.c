63b58e9c1371748b54d762cec8255fcdb4410749
leitner <leitner>
2015-04-11 04:22:26 +0000

    remove compiler warnings

--- lib/execl.c
+++ lib/execl.c
@@ -4,18 +4,20 @@
 #include <stdlib.h>
 #include "dietfeatures.h"
 
-int execl( const char *path,...) {
+int execl( const char *path, const char* arg, ...) {
   va_list ap,bak;
   int n,i;
   char **argv,*tmp;
-  va_start(ap, path);
+  (void)arg;
+  va_start(ap, arg);
   va_copy(bak,ap);
-  n=1;
+  n=2;
   while ((tmp=va_arg(ap,char *)))
     ++n;
   va_end (ap);
   if ((argv=(char **)alloca(n*sizeof(char*)))) {
-    for (i=0; i<n; ++i)
+    argv[0]=(char*)arg;
+    for (i=1; i<n; ++i)
       argv[i]=va_arg(bak,char *);
     va_end (bak);
     return execve(path,argv,environ);
--- lib/execle.c
+++ lib/execle.c
@@ -4,18 +4,20 @@
 #include <stdlib.h>
 #include "dietfeatures.h"
 
-int execle( const char *path,...) {
+int execle( const char *path, const char* arg, ...) {
   va_list ap;
   int n,i;
   char **argv,*tmp, **env;
-  va_start(ap, path);
-  n=1;
+  va_start(ap, arg);
+  (void)arg;
+  n=2;
   while ((tmp=va_arg(ap,char *)))
     ++n;
   va_end (ap);
   if ((argv=(char **)alloca(n*sizeof(char*)))) {
-    va_start(ap, path);
-    for (i=0; i<n; ++i)
+    va_start(ap, arg);
+    argv[0]=(char*)arg;
+    for (i=1; i<n; ++i)
       argv[i]=va_arg(ap,char *);
     env=va_arg(ap, char **);
     va_end (ap);
