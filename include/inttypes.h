fb2f5903bf9ddf154be2f49f073d3368c4e5c4ce
leitner <leitner>
2015-06-28 20:35:14 +0000

    define uintptr_t using __UINTPTR_TYPE__ (32-bit platforms may need a make clean)

--- include/inttypes.h
+++ include/inttypes.h
@@ -252,8 +252,8 @@ typedef unsigned int uint32_t __attribute__((__mode__(__SI__)));
 typedef unsigned int uint32_t;
 #endif
 
-typedef signed long int intptr_t;
-typedef unsigned long int uintptr_t;
+typedef __INTPTR_TYPE__ intptr_t;
+typedef __UINTPTR_TYPE__ uintptr_t;
 
 #if __WORDSIZE == 64
 typedef signed long int64_t;
