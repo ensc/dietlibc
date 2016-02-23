e0678f184b63b956dd423b3d56cb77cd9b6c5805
leitner <leitner>
2016-02-23 22:05:01 +0000

    more than one thread can thrd_join on the same thread

--- lib/thrd_join.c
+++ lib/thrd_join.c
@@ -8,7 +8,14 @@
 int thrd_join(thrd_t thr, int* res) {
   int flags=thr->flags;
   int r;
-  if ((flags&2)==0) {
+  while ((flags&6) == 4) {
+    /* if flags&4 then thrd_exit is already working on that thread.
+     * when it's done, it does flags|=2.
+     * so we wait for that. */
+    __sync_synchronize();
+    flags=thr->flags;
+  }
+  if ((flags&2)==0) {	/* is the thread still running? */
     if (flags&5)
       // thread is detached or in the process of being cleaned up, refuse join
       return thrd_error;
@@ -22,6 +29,7 @@ int thrd_join(thrd_t thr, int* res) {
     thr->join_wait_futex=1;
     futex(&thr->join_wait_futex,FUTEX_WAKE,1,0,0,0);
   } else {
+    /* thread is already dead, do cleanup */
     if (res) *res=thr->res;
     munmap(thr->arg,thr->memsize);
   }
