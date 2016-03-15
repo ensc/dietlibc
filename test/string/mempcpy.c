03a2cf55b97637eda7b51362a5337020c75c2754
Enrico Scholz <enrico.scholz@ensc.de>
2016-03-15 19:53:09 +0100

    TESTS: improved stpncpy() to work with non posix compliant implmentation
    Test has been enhanced to check whether boundaries are not
    overridden.
    
    Signed-off-by: Enrico Scholz <enrico.scholz@ensc.de>

--- test/string/mempcpy.c
+++ test/string/mempcpy.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <assert.h>
 #include <sys/param.h>
+#include "../../dietfeatures.h"
 
 int main() {
   char test[100]="blubber";
@@ -13,7 +14,9 @@ int main() {
 
   stpncpy(test,"bar",2);
   assert(stpncpy(test,"bar",2)==test+2 && !strcmp(test,"bao"));
-  assert(stpncpy(test,"xyz",6)==test+3 && !memcmp(test,"xyz\0\0\0",6));
+#ifdef WANT_FULL_POSIX_COMPAT
+  assert(stpncpy(test,"xyz",6)==test+3 && !memcmp(test,"xyz\0\0\0r",7));
+#endif
 
   return 0;
 }
