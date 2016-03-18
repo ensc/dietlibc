b085440e0f4a87ec0596574363946213832565b2
leitner <leitner>
2016-03-18 18:20:05 +0000

    compile fix (typo)

--- lib/stpncpy.c
+++ lib/stpncpy.c
@@ -4,7 +4,7 @@
 char* stpncpy(char* dest, const char* src, size_t n) {
   char* x=memccpy(dest,src,0,n);
   if (x) {
-    memset(x,0,n-(x-dst));
+    memset(x,0,n-(x-dest));
     return x-1;
   } else
     return dest+n;
