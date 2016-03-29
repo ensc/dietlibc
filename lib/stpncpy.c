8ec2c2f398ada4831bee1d5449f4c9ad86cfd226
leitner <leitner>
2016-03-29 15:01:30 +0000

    make zero-filling optional

--- lib/stpncpy.c
+++ lib/stpncpy.c
@@ -1,10 +1,13 @@
 #define _GNU_SOURCE
 #include <string.h>
+#include "dietfeatures.h"
 
 char* stpncpy(char* dest, const char* src, size_t n) {
   char* x=memccpy(dest,src,0,n);
   if (x) {
-    memset(x,0,n-(x-dest));
+#ifdef WANT_FULL_POSIX_COMPAT
+    memset(x,0,n-(x-dst));
+#endif
     return x-1;
   } else
     return dest+n;
