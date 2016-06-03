8aa60cd99f3552a378c2d949e0b7dce4f6b7f26f
leitner <leitner>
2016-06-03 15:45:00 +0000

    typo

--- lib/stpncpy.c
+++ lib/stpncpy.c
@@ -6,7 +6,7 @@ char* stpncpy(char* dest, const char* src, size_t n) {
   char* x=memccpy(dest,src,0,n);
   if (x) {
 #ifdef WANT_FULL_POSIX_COMPAT
-    memset(x,0,n-(x-dst));
+    memset(x,0,n-(x-dest));
 #endif
     return x-1;
   } else
