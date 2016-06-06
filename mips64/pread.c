a1adc8e10c84e696c34a286db78ecd86f7fdbf50
leitner <leitner>
2016-06-06 13:13:12 +0000

    add a few missing files

---
+++ mips64/pread.c
@@ -0,0 +1,2 @@
+/* mips32 has its own implementation for pread, but mips64 can use the generic code */
+#include "../lib/pread.c"
---
+++ mips64/pwrite.c
@@ -0,0 +1,2 @@
+/* mips32 has its own implementation for pwrite, but mips64 can use the generic code */
+#include "../lib/pwrite.c"
