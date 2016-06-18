7a5bfa3f9fb644660047399da26e763cdfe81d67
Enrico Scholz <enrico.scholz@ensc.de>
2016-06-18 13:07:41 +0200

    ppoll: declare __ppoll
    fixes
    
    | lib/ppoll.c: In function 'ppoll':
    | lib/ppoll.c:9:9: error: implicit declaration of function '__ppoll' [-Werror=implicit-function-declaration]
    |   return __ppoll(fds, nfds, timeout, sigmask, _NSIG/8);
    
    Signed-off-by: Enrico Scholz <enrico.scholz@ensc.de>

--- lib/ppoll.c
+++ lib/ppoll.c
@@ -3,6 +3,10 @@
 #include "syscalls.h"
 
 #ifdef __NR_ppoll
+extern int __ppoll(struct pollfd* fds, nfds_t nfds,
+		   const struct timespec *timeout,
+		   const sigset_t* sigmask, size_t sigsetsize);
+
 int ppoll(struct pollfd* fds, nfds_t nfds, const struct timespec *timeout,
 		const sigset_t* sigmask)
 {
