b3547d7ae6af7fe482f5209061e20fddf8afeefe
leitner <leitner>
2017-03-06 13:41:51 +0000

    quick fix for weak syscalls with number > 255

--- i386/syscalls.h
+++ i386/syscalls.h
@@ -391,8 +391,13 @@ wsym: ; \
 .type sym,@function; \
 .global sym; \
 sym: \
+.ifle __NR_##name-255; \
 	movb $__NR_##name,%al; \
 	jmp __unified_syscall; \
+.else; \
+	movw $__NR_##name,%ax; \
+	jmp __unified_syscall_256; \
+.endif; \
 .Lend##sym: ; \
 .size sym,.Lend##sym-sym
 
